b8b80860e0919ea521ca33db721515148ce23182
sakurawald <sakurawald@gmail.com>
2025-06-10 09:56:53 +0800

    fix: add the conditional imports for mc1.21.4

--- fabric/src/main/java/io/github/sakurawald/module/mixin/command_toolbox/hat/LivingEntityMixin.java
+++ fabric/src/main/java/io/github/sakurawald/module/mixin/command_toolbox/hat/LivingEntityMixin.java
@@ -3,6 +3,14 @@ package io.github.sakurawald.module.mixin.command_toolbox.hat;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 
+#if MC_VER >= MC_1_21_5
+import net.minecraft.entity.EquipmentSlot;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.item.ItemStack;
+#endif
+
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
 
