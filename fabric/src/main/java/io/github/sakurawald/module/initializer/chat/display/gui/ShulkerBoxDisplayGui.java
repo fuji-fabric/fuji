70789f670c7a1ce3f579493e79fbdf963699545c
sakurawald <sakurawald@gmail.com>
2025-06-11 10:24:53 +0800

    chore: backport for MC 1.20.4 (extractor for shulker box)

--- fabric/src/main/java/io/github/sakurawald/module/initializer/chat/display/gui/ShulkerBoxDisplayGui.java
+++ fabric/src/main/java/io/github/sakurawald/module/initializer/chat/display/gui/ShulkerBoxDisplayGui.java
@@ -2,13 +2,22 @@ package io.github.sakurawald.module.initializer.chat.display.gui;
 
 import eu.pb4.sgui.api.gui.SimpleGui;
 import io.github.sakurawald.core.auxiliary.minecraft.GuiHelper;
+#if MC_VER <= MC_1_20_4
+#elif MC_VER > MC_1_20_4
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.ContainerComponent;
+#endif
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.collection.DefaultedList;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.stream.Stream;
 
 
 public class ShulkerBoxDisplayGui extends BaseDisplayGui {
@@ -21,6 +30,37 @@ public class ShulkerBoxDisplayGui extends BaseDisplayGui {
         this.title = title;
         this.itemStack = itemStack;
         this.parentGui = parentGui;
+    }
+
+    private static @NotNull Stream<ItemStack> extractItemListFromShulkerBox(ItemStack stack) {
+
+        #if MC_VER <= MC_1_20_4
+        NbtCompound blockEntityData = BlockItem.getBlockEntityNbt(stack);
+        if (blockEntityData != null) {
+            NbtList items = (NbtList) blockEntityData.get("Items");
+            if (items == null) return Stream.empty();
+
+            DefaultedList<ItemStack> temp = DefaultedList.ofSize(items.size(), ItemStack.EMPTY);
+
+            items.forEach(item -> {
+                NbtCompound itemNbtCompound = (NbtCompound) item;
+                int slotIndex = itemNbtCompound.getInt("Slot");
+                ItemStack itemStack = ItemStack.fromNbt(itemNbtCompound);
+
+                temp.set(slotIndex, itemStack);
+            });
+            return temp.stream();
+        }
+        return Stream.empty();
+
+        #elif MC_VER > MC_1_20_4
+        ContainerComponent containerComponent = stack.get(DataComponentTypes.CONTAINER);
+        if (containerComponent == null) {
+            return Stream.empty();
+        }
+        return containerComponent.stream();
+        #endif
+
     }
 
     @Override
@@ -39,17 +79,15 @@ public class ShulkerBoxDisplayGui extends BaseDisplayGui {
         }
 
         /* construct items */
-        ContainerComponent containerComponent = itemStack.get(DataComponentTypes.CONTAINER);
+        Stream<ItemStack> containerStream = extractItemListFromShulkerBox(itemStack);
 
-        if (containerComponent != null) {
-            var counter = new Object() {
-                int offset = 0;
-            };
-            containerComponent.stream().forEach(item -> {
-                gui.setSlot(LINE_SIZE + counter.offset, item.copy());
-                counter.offset++;
-            });
-        }
+        var counter = new Object() {
+            int offset = 0;
+        };
+        containerStream.forEach(item -> {
+            gui.setSlot(LINE_SIZE + counter.offset, item.copy());
+            counter.offset++;
+        });
 
         return gui;
     }
