58f04fde7a996f46d4c6a2c48c3c2b8b0cd1ff14
sakurawald <sakurawald@gmail.com>
2025-06-11 12:12:57 +0800

    chore: backport for MC 1.20.4 (compass module)

--- fabric/src/main/java/io/github/sakurawald/module/initializer/command_toolbox/compass/CompassInitializer.java
+++ fabric/src/main/java/io/github/sakurawald/module/initializer/command_toolbox/compass/CompassInitializer.java
@@ -3,22 +3,30 @@ package io.github.sakurawald.module.initializer.command_toolbox.compass;
 import io.github.sakurawald.core.annotation.Document;
 import io.github.sakurawald.core.auxiliary.minecraft.CommandHelper;
 import io.github.sakurawald.core.auxiliary.minecraft.EntityHelper;
+import io.github.sakurawald.core.auxiliary.minecraft.NbtHelper;
+import io.github.sakurawald.core.auxiliary.minecraft.RegistryHelper;
 import io.github.sakurawald.core.auxiliary.minecraft.TextHelper;
 import io.github.sakurawald.core.command.annotation.CommandNode;
 import io.github.sakurawald.core.command.annotation.CommandSource;
 import io.github.sakurawald.core.command.annotation.CommandTarget;
 import io.github.sakurawald.core.command.argument.wrapper.impl.Dimension;
 import io.github.sakurawald.module.initializer.ModuleInitializer;
+#if MC_VER <= MC_1_20_4
+#elif MC_VER > MC_1_20_4
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.LodestoneTrackerComponent;
+import net.minecraft.util.math.GlobalPos;
+import java.util.Optional;
+#endif
+
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.GlobalPos;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.Optional;
 import java.util.function.Function;
 
 @CommandNode("compass")
@@ -34,9 +42,33 @@ public class CompassInitializer extends ModuleInitializer {
         return function.apply(itemStack);
     }
 
-    private static void setTrackedTarget(ItemStack itemStack, ServerWorld world, BlockPos blockPos) {
+    private static void setTrackedTarget(ItemStack itemStack, @Nullable ServerWorld world, @Nullable BlockPos blockPos) {
+
+        #if MC_VER <= MC_1_20_4
+        NbtHelper.withNbt(itemStack, tag -> {
+            if (world == null) {
+                tag.remove("LodestoneDimension");
+            } else {
+                tag.putString("LodestoneDimension", RegistryHelper.ofString(world));
+            }
+
+            if (blockPos == null) {
+                tag.remove("LodestonePos");
+            } else {
+                NbtCompound posTag = new NbtCompound();
+                posTag.putInt("X", blockPos.getX());
+                posTag.putInt("Y", blockPos.getY());
+                posTag.putInt("Z", blockPos.getZ());
+                tag.put("LodestonePos", posTag);
+            }
+
+        });
+
+        #elif MC_VER > MC_1_20_4
         LodestoneTrackerComponent component = new LodestoneTrackerComponent(Optional.of(GlobalPos.create(world.getRegistryKey(), blockPos)), false);
         itemStack.set(DataComponentTypes.LODESTONE_TRACKER, component);
+        #endif
+
     }
 
     @CommandNode("track pos")
@@ -61,7 +93,7 @@ public class CompassInitializer extends ModuleInitializer {
     @Document("Let the compass in hand track nothing.")
     private static int reset(@CommandSource @CommandTarget ServerPlayerEntity player) {
         return withCompassInHand(player, (itemStack) -> {
-            itemStack.set(DataComponentTypes.LODESTONE_TRACKER, null);
+            setTrackedTarget(itemStack,null,null);
             return CommandHelper.Return.SUCCESS;
         });
     }
