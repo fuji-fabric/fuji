c5726443767f820c4cd42423d1397d807139beb2
sakurawald <sakurawald@gmail.com>
2025-06-11 14:30:59 +0800

    fix: conditional imports between mc1.20.3 and mc1.20.4

--- fabric/src/main/java/io/github/sakurawald/module/mixin/chat/style/message_type/RegistryLoaderMixin.java
+++ fabric/src/main/java/io/github/sakurawald/module/mixin/chat/style/message_type/RegistryLoaderMixin.java
@@ -9,7 +9,11 @@ import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryLoader;
+#if MC_VER < MC_1_20_4
 import net.minecraft.resource.ResourceManager;
+#elif MC_VER >= MC_1_20_4
+import org.spongepowered.asm.mixin.injection.Coerce;
+#endif
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
