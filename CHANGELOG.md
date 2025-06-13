41d94e0fcd93b12a18f46086289769315cbdec38
sakurawald <sakurawald@gmail.com>
2025-06-14 01:26:18 +0800

    fix: should not process the command spy twice in MC 1.20.1~1.20.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 > The version number of fuji follows `semver` now: https://semver.org/
 
-- (MC 1.20.1) [command_advice] fix: should not process the command advice twice.
+- (MC 1.20.1 ~ MC 1.20.2) [command_advice] fix: should not process the command advice twice.
+- (MC 1.20.1 ~ MC 1.20.2) [command_spy] fix: should not process the command spy twice.
--- fabric/src/main/java/io/github/sakurawald/module/mixin/command_spy/CommandManagerMixin.java
+++ fabric/src/main/java/io/github/sakurawald/module/mixin/command_spy/CommandManagerMixin.java
@@ -1,31 +1,31 @@
 package io.github.sakurawald.module.mixin.command_spy;
 
-import com.mojang.brigadier.ParseResults;
-import io.github.sakurawald.module.initializer.command_spy.CommandSpyInitializer;
 import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.command.ServerCommandSource;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 
 #if MC_VER <= MC_1_20_2
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 #elif MC_VER > MC_1_20_2
+import net.minecraft.server.command.ServerCommandSource;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.jetbrains.annotations.NotNull;
+import com.mojang.brigadier.ParseResults;
+import io.github.sakurawald.module.initializer.command_spy.CommandSpyInitializer;
 #endif
 
 
 @Mixin(CommandManager.class)
 public class CommandManagerMixin {
 
-    @Inject(method = "execute", at = @At("HEAD"))
     #if MC_VER <= MC_1_20_2
-    public void onExecuteInCommandManager(ParseResults<ServerCommandSource> parseResults, String string, CallbackInfoReturnable<Integer> cir)
+    // See command advice module.
     #elif MC_VER > MC_1_20_2
+    @Inject(method = "execute", at = @At("HEAD"))
     public void onExecuteInCommandManager(@NotNull ParseResults<ServerCommandSource> parseResults, String string, CallbackInfo ci)
-    #endif
     {
         CommandSpyInitializer.processCommandSpy(parseResults);
     }
+    #endif
 }
