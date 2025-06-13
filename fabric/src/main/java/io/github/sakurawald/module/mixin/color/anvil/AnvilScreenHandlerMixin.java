c8f5febad2f67f287fc87f209aed1f0aae136c73
sakurawald <sakurawald@gmail.com>
2025-06-13 14:13:32 +0800

    [publish] v7.2.2

--- fabric/src/main/java/io/github/sakurawald/module/mixin/color/anvil/AnvilScreenHandlerMixin.java
+++ fabric/src/main/java/io/github/sakurawald/module/mixin/color/anvil/AnvilScreenHandlerMixin.java
@@ -11,12 +11,8 @@ import net.minecraft.screen.ScreenHandlerType;
 #if MC_VER > MC_1_21
 import net.minecraft.screen.slot.ForgingSlotsManager;
 #endif
-#if MC_VER <= MC_1_20_4
-import net.minecraft.text.Text;
-#elif MC_VER > MC_1_20_4
-import org.jetbrains.annotations.NotNull;
-#endif
 
+import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
