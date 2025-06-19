9b3eb6065a60a9d20cceeb3d063b41a104b49228
sakurawald <sakurawald@gmail.com>
2025-06-20 04:12:30 +0800

    feature: use `send-title` to display command_warmup messages.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 > Read detailed change logs in https://github.com/sakurawald/fuji/commits/dev/
 
 - [top_chunks] feature: new GUI interface to display top chunks. (Include a searching function)
-- [command_cooldown] feature: use `title` interface to display the cooldown message.
+- [command_cooldown] feature: use `title` interface to display the message.
+- [command_warmup] feature: use `title` interface to display the message.
 
--- fabric/src/main/java/io/github/sakurawald/module/mixin/command_warmup/ServerPlayNetworkHandlerMixin.java
+++ fabric/src/main/java/io/github/sakurawald/module/mixin/command_warmup/ServerPlayNetworkHandlerMixin.java
@@ -6,11 +6,10 @@ import io.github.sakurawald.core.structure.Tag;
 import io.github.sakurawald.module.initializer.command_warmup.CommandWarmupInitializer;
 import io.github.sakurawald.module.initializer.command_warmup.structure.CommandWarmupNode;
 import io.github.sakurawald.module.initializer.command_warmup.structure.CommandWarmupTicket;
-import net.minecraft.network.message.LastSeenMessageList;
 import net.minecraft.network.packet.c2s.play.CommandExecutionC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -43,7 +42,8 @@ public abstract class ServerPlayNetworkHandlerMixin {
 
                 // Send warning for movement.
                 if (config.warn_for_move) {
-                    TextHelper.sendActionBarByKey(player, "command_warmup.warn_for_move", entry.getInterruptible().getInterruptDistance());
+                    Text text = TextHelper.getTextByKey(player, "command_warmup.warn_for_move", entry.getInterruptible().getInterruptDistance());
+                    TextHelper.sendTitle(player, text, Text.empty());
                 }
 
                 // Cancel the issue of command string.
