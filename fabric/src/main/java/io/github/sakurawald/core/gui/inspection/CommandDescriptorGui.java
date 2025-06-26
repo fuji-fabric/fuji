b1cbfd55f4a66dabf750cfff7dae07e0cc0749ac
sakurawald <sakurawald@gmail.com>
2025-06-26 10:11:09 +0800

    refactor: extract the interface SourceModuleGetter.

--- fabric/src/main/java/io/github/sakurawald/core/gui/inspection/CommandDescriptorGui.java
+++ fabric/src/main/java/io/github/sakurawald/core/gui/inspection/CommandDescriptorGui.java
@@ -66,7 +66,7 @@ public class CommandDescriptorGui extends PagedGui<CommandDescriptor> {
 
         /* Add basic properties of command descriptor. */
         lore.addAll(List.of(
-            TextHelper.getTextByKey(getPlayer(),"from_module", entity.getSourceModulePath())
+            TextHelper.getTextByKey(getPlayer(),"from_module", entity.getSourceModule())
             , TextHelper.getTextByKey(getPlayer(), "command.source.can_be_executed_by_console", entity.canBeExecutedByConsole())
             , TextHelper.getTextByKey(getPlayer(), "command.descriptor.type", entity.getClass().getSimpleName())
             , TextHelper.getTextByKey(getPlayer(), "command.requirement.level_permission", entity.getDefaultLevelPermission())
