5d777b8fb48d13b41799b9589f2f0d9ecb31a25e
sakurawald <sakurawald@gmail.com>
2025-06-27 06:07:07 +0800

    feature: add the module initializer for the `core`.

---
+++ fabric/src/main/java/io/github/sakurawald/module/initializer/core/CoreInitializer.java
@@ -0,0 +1,10 @@
+package io.github.sakurawald.module.initializer.core;
+
+import io.github.sakurawald.core.structure.descriptor.annotation.ColorBox;
+import io.github.sakurawald.module.initializer.ModuleInitializer;
+
+@ColorBox("This is the color box from core")
+public class CoreInitializer extends ModuleInitializer {
+
+
+}
