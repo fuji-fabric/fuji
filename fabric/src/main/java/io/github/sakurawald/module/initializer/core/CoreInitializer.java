d4fbfb711030895801529b330615cedd3c5e1cd0
sakurawald <sakurawald@gmail.com>
2025-06-27 09:24:22 +0800

    [publish] v9.13.0

--- fabric/src/main/java/io/github/sakurawald/module/initializer/core/CoreInitializer.java
+++ fabric/src/main/java/io/github/sakurawald/module/initializer/core/CoreInitializer.java
@@ -3,7 +3,7 @@ package io.github.sakurawald.module.initializer.core;
 import io.github.sakurawald.core.structure.descriptor.annotation.ColorBox;
 import io.github.sakurawald.module.initializer.ModuleInitializer;
 
-@ColorBox("This is the color box from core")
+@ColorBox("More color boxes coming.")
 public class CoreInitializer extends ModuleInitializer {
 
 
