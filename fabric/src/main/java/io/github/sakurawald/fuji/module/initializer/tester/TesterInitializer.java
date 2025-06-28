be1524ec0b1a1d1a943a3ef81fc989e31273e6bb
sakurawald <sakurawald@gmail.com>
2025-06-29 04:37:50 +0800

    refactor: move test functions to its class.

--- fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/tester/TesterInitializer.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/tester/TesterInitializer.java
@@ -3,20 +3,27 @@ package io.github.sakurawald.fuji.module.initializer.tester;
 
 import com.mojang.brigadier.context.CommandContext;
 import io.github.sakurawald.fuji.core.auxiliary.LogUtil;
-import io.github.sakurawald.fuji.core.auxiliary.minecraft.ServerHelper;
-import io.github.sakurawald.fuji.core.auxiliary.minecraft.TextHelper;
 import io.github.sakurawald.fuji.core.command.annotation.CommandNode;
 import io.github.sakurawald.fuji.core.command.annotation.CommandRequirement;
 import io.github.sakurawald.fuji.core.command.annotation.CommandSource;
+import io.github.sakurawald.fuji.core.manager.Managers;
 import io.github.sakurawald.fuji.core.manager.impl.module.ModuleManager;
 import io.github.sakurawald.fuji.module.initializer.ModuleInitializer;
+import io.github.sakurawald.fuji.module.initializer.tester.functions.TestFunctions;
 import lombok.SneakyThrows;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.TriggerKey;
+import org.quartz.impl.matchers.GroupMatcher;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 
 @CommandNode("tester")
@@ -28,65 +35,60 @@ public class TesterInitializer extends ModuleInitializer {
     @SneakyThrows(Exception.class)
     @CommandNode("run")
     private static int $run(@CommandSource ServerPlayerEntity player) {
-//        LogUtil.info(LogUtil.findSourceModuleInCurrentStack());
+
+        Scheduler scheduler = Managers.getScheduleManager().getScheduler();
+
+        List<String> jobGroupNames = scheduler.getJobGroupNames();
+        jobGroupNames.forEach(it -> LogUtil.info("jobGroupName = {}", it));
+
+        Set<JobKey> jobKeys = scheduler.getJobKeys(GroupMatcher.anyGroup());
+        jobKeys.forEach(it -> {
+            LogUtil.info("jobKey = {}", it);
+
+            try {
+                List<? extends Trigger> triggersOfJob = scheduler.getTriggersOfJob(it);
+
+                triggersOfJob.forEach(trigger -> {
+                    LogUtil.info("trigger = {}", trigger);
+                    Date nextFireTime = trigger.getNextFireTime();
+                    LogUtil.info("nextFireTime = {}", nextFireTime);
+                });
+
+            } catch (SchedulerException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+
+        List<String> triggerGroupNames = scheduler.getTriggerGroupNames();
+        LogUtil.info("triggerGroupNames = {}", triggerGroupNames);
+        Set<TriggerKey> triggerKeys = scheduler.getTriggerKeys(GroupMatcher.anyGroup());
+        LogUtil.info("triggerKeys = {}", triggerKeys);
+//        scheduler.trigger
+
         return 0;
-    }
-
-    @CommandNode("world")
-    private static int testWorld(@CommandSource ServerPlayerEntity playerEntity) {
-        MinecraftServer server = ServerHelper.getServer();
-        server.getPlayerManager();
-        return 1;
-    }
-
-    @CommandNode("visible")
-    private static int testInvisible(@CommandSource ServerPlayerEntity player) {
-        player.sendMessage(Text.of(String.valueOf(player.isInvisible())));
-        return 1;
     }
 
     @CommandNode("text-replace")
     private static int testTextReplace(@CommandSource ServerPlayerEntity player) {
-        testTextReplacement(player);
+        TestFunctions.testTextReplacement(player);
         return 1;
     }
 
-    private static void testTextReplacement(ServerPlayerEntity player) {
-        /* make */
-        MutableText root = Text.empty();
-
-        MutableText first = Text.literal("first").formatted(Formatting.RED);
-        root.append(first);
-
-        MutableText first_first = Text.literal("second").formatted(Formatting.GREEN);
-        first.append(first_first);
-
-        MutableText first_second = Text.literal("third");
-        first.append(first_second);
-
-        /* replace */
-        LogUtil.debug("before = {}", root);
-        player.sendMessage(root);
-
-        MutableText after = TextHelper.replaceTextWithRegex(root, "hi", () -> Text.literal("{replacement}"));
-        LogUtil.debug("after = {}", after);
-        player.sendMessage(after);
-    }
-
     @CommandNode("$1 minus $2")
-    private static int $2(@CommandSource ServerPlayerEntity player, Integer a, Integer b) {
+    private static int $argumentReference(@CommandSource ServerPlayerEntity player, Integer a, Integer b) {
         player.sendMessage(Text.of(String.valueOf(a - b)));
         return 1;
     }
 
     @CommandNode("ctx")
-    private static int ctx(@CommandSource CommandContext<ServerCommandSource> ctx) {
+    private static int $ctx(@CommandSource CommandContext<ServerCommandSource> ctx) {
         ctx.getSource().sendMessage(Text.of("root"));
         return 1;
     }
 
     @CommandNode
-    private static int root(@CommandSource ServerPlayerEntity player) {
+    private static int $root(@CommandSource ServerPlayerEntity player) {
         player.sendMessage(Text.of("root"));
         return 1;
     }
