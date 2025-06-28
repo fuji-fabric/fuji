c19af0b53a49349aeb40e5d1fdb34402494f12ba
sakurawald <sakurawald@gmail.com>
2025-06-28 15:04:07 +0800

    fix: package import

--- fabric/src/main/java/io/github/sakurawald/fuji/core/command/argument/adapter/impl/TextArgumentTypeAdapter.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/command/argument/adapter/impl/TextArgumentTypeAdapter.java
@@ -4,7 +4,10 @@ import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import io.github.sakurawald.fuji.core.command.argument.adapter.abst.BaseArgumentTypeAdapter;
 import io.github.sakurawald.fuji.core.command.argument.structure.Argument;
+#if MC_VER <= MC_1_20_4
+#elif MC_VER > MC_1_20_4
 import io.github.sakurawald.fuji.core.command.processor.CommandAnnotationProcessor;
+#endif
 import net.minecraft.command.argument.TextArgumentType;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
