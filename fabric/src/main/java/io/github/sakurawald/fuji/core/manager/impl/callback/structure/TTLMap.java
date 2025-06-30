636576bfeafc7bf2d27b7eaaae9fbc73b464c019
sakurawald <sakurawald@gmail.com>
2025-06-30 16:07:22 +0800

    refactor: clean up for TTL map.

--- fabric/src/main/java/io/github/sakurawald/fuji/core/manager/impl/callback/structure/TTLMap.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/manager/impl/callback/structure/TTLMap.java
@@ -15,10 +15,9 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 public class TTLMap<K, V> {
-    private final ConcurrentMap<K, ExpiringValue<V>> map = new ConcurrentHashMap<>();
+    private final ConcurrentMap<K, ExpiringValue<V>> backendMap = new ConcurrentHashMap<>();
 
     public TTLMap() {
-        // do the cleanup every minute.
         CleanTTLMapJob cleanTTLMapJob = new CleanTTLMapJob(new JobDataMap() {
             {
                 this.put(TTLMap.class.getName(), TTLMap.this);
@@ -29,38 +28,36 @@ public class TTLMap<K, V> {
 
     public void put(K key, V value, long ttl, TimeUnit unit) {
         long expiryTime = System.currentTimeMillis() + unit.toMillis(ttl);
-        map.put(key, new ExpiringValue<>(value, expiryTime));
+        backendMap.put(key, new ExpiringValue<>(value, expiryTime));
     }
 
     public V get(K key) {
-        ExpiringValue<V> expiringValue = map.get(key);
+        ExpiringValue<V> expiringValue = backendMap.get(key);
         if (expiringValue != null && System.currentTimeMillis() < expiringValue.expiryTime) {
             return expiringValue.value;
         } else {
-            map.remove(key);
+            backendMap.remove(key);
             return null;
         }
     }
 
     public void remove(K key) {
-        map.remove(key);
+        backendMap.remove(key);
     }
 
     private void cleanUp() {
         long currentTime = System.currentTimeMillis();
-        for (Map.Entry<K, ExpiringValue<V>> entry : map.entrySet()) {
+        for (Map.Entry<K, ExpiringValue<V>> entry : backendMap.entrySet()) {
             if (currentTime > entry.getValue().expiryTime) {
-                map.remove(entry.getKey());
+                backendMap.remove(entry.getKey());
             }
         }
     }
 
-    private record ExpiringValue<V>(V value, long expiryTime) {
-    }
-
+    private record ExpiringValue<V>(V value, long expiryTime) {}
 
     @Document("""
-        This `job` is used to clean the `map` data structure, based on `TTL`.
+        This `job` is used to clean up the `TTL Map` data structure, and remove `expired entries`.
         """)
     @NoArgsConstructor
     public static class CleanTTLMapJob extends CronJob {
