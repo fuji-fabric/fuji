9dd4cdf97798d85c129af9ed01beed0c4872079a
sakurawald <sakurawald@gmail.com>
2025-07-07 01:33:30 +0800

    fix: package import

--- fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/command_toolbox/compass/CompassInitializer.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/command_toolbox/compass/CompassInitializer.java
@@ -10,6 +10,9 @@ import io.github.sakurawald.fuji.core.command.annotation.CommandTarget;
 import io.github.sakurawald.fuji.core.command.argument.wrapper.impl.Dimension;
 import io.github.sakurawald.fuji.module.initializer.ModuleInitializer;
 #if MC_VER <= MC_1_20_4
+import net.minecraft.nbt.NbtCompound;
+import io.github.sakurawald.fuji.core.auxiliary.minecraft.RegistryHelper;
+import io.github.sakurawald.fuji.core.auxiliary.minecraft.ItemStackHelper;
 #elif MC_VER > MC_1_20_4
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.LodestoneTrackerComponent;
