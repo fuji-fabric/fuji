7127c7d4f1dd5cfe1d62d854c76538643f47e252
sakurawald <sakurawald@gmail.com>
2025-07-06 16:26:48 +0800

    refactor: document for getText()

--- fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/tester/TesterInitializer.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/tester/TesterInitializer.java
@@ -2,21 +2,43 @@ package io.github.sakurawald.fuji.module.initializer.tester;
 
 
 import com.mojang.brigadier.context.CommandContext;
+import eu.pb4.common.economy.api.CommonEconomy;
+import eu.pb4.common.economy.api.EconomyAccount;
+import eu.pb4.common.economy.api.EconomyCurrency;
+import eu.pb4.common.economy.api.EconomyProvider;
 import io.github.sakurawald.fuji.core.auxiliary.LogUtil;
-import io.github.sakurawald.fuji.core.auxiliary.minecraft.RegistryHelper;
 import io.github.sakurawald.fuji.core.command.annotation.CommandNode;
 import io.github.sakurawald.fuji.core.command.annotation.CommandRequirement;
 import io.github.sakurawald.fuji.core.command.annotation.CommandSource;
+import io.github.sakurawald.fuji.core.document.annotation.ColorBox;
 import io.github.sakurawald.fuji.core.manager.impl.module.ModuleManager;
 import io.github.sakurawald.fuji.module.initializer.ModuleInitializer;
 import io.github.sakurawald.fuji.module.initializer.tester.functions.TestFunctions;
 
 import lombok.SneakyThrows;
-import net.minecraft.item.Item;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
+
+@ColorBox(color = ColorBox.ColorBlockTypes.TIPS, value = """
+  <json>
+  "core": {
+    "debug": {
+      "disable_all_modules": false,
+      "log_debug_messages": false,
+      "print_user_guide_in_console": false
+    },
+    "backup": {
+      "max_slots": 15,
+      "skip": [
+        "modules/head"
+      ]
+    },
+  </json>
+
+  """)
 
 @CommandNode("tester")
 @CommandRequirement(level = 4)
@@ -28,8 +50,15 @@ public class TesterInitializer extends ModuleInitializer {
     @CommandNode("run")
     private static int $run(@CommandSource ServerPlayerEntity player) {
 
-        Item item = RegistryHelper.ofItem("asd:asd");
-        LogUtil.warn("item = {}", item);
+        EconomyProvider fuji = CommonEconomy.getProvider("fuji");
+        LogUtil.info("provider = {}", fuji);
+
+        EconomyCurrency currency = CommonEconomy.getCurrency(player.getServer(), Identifier.of("fuji:gold"));
+        LogUtil.info("first currency = {}", currency);
+
+        EconomyAccount account = CommonEconomy.getAccount(player, Identifier.of("fuji:gold"));
+        LogUtil.info("account = {}", account);
+
 
         return 0;
     }
