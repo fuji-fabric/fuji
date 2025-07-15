dd32c9611034c2903bd808b7e95bc9109b99d585
sakurawald <sakurawald@gmail.com>
2025-07-15 16:24:25 +0800

    docs: ad doc string for world.border

--- fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/world/border/structure/BorderDescriptor.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/world/border/structure/BorderDescriptor.java
@@ -1,28 +1,35 @@
 package io.github.sakurawald.fuji.module.initializer.world.border.structure;
 
+import io.github.sakurawald.fuji.core.document.annotation.Document;
 import lombok.Data;
 import net.minecraft.world.border.WorldBorder;
 
+@Document(id = 1752567811335L, value = """
+    Used to describe the `border` of a `dimension`.
+    """)
 @Data
 public class BorderDescriptor {
 
     public final String dimensionId;
-    public double centerX = 0.0;
-    public double centerZ = 0.0;
-    public double size = 9999968E7;
-    public long sizeLerpTime = 0L;
-    public double sizeLerpTarget = 0.0;
-    public int warningBlocks = 5;
-    public int warningTime = 15;
-    public double damagePerBlock = 0.2;
-    public double safeZone = 5.0;
+    public final Border border = new Border();
+    public static class Border {
+        public double centerX = 0.0;
+        public double centerZ = 0.0;
+        public double size = 9999968E7;
+        public long sizeLerpTime = 0L;
+        public double sizeLerpTarget = 0.0;
+        public int warningBlocks = 5;
+        public int warningTime = 15;
+        public double damagePerBlock = 0.2;
+        public double safeZone = 5.0;
+    }
 
     private transient WorldBorder vanillaWorldBorder;
 
     public WorldBorder asVanillaWorldBorder() {
         if (this.vanillaWorldBorder == null) {
             WorldBorder worldBorder = new WorldBorder();
-            WorldBorder.Properties properties = new WorldBorder.Properties(this.centerX, this.centerZ, this.damagePerBlock, this.safeZone, this.warningBlocks, this.warningTime, this.size, this.sizeLerpTime, this.sizeLerpTarget);
+            WorldBorder.Properties properties = new WorldBorder.Properties(this.border.centerX, this.border.centerZ, this.border.damagePerBlock, this.border.safeZone, this.border.warningBlocks, this.border.warningTime, this.border.size, this.border.sizeLerpTime, this.border.sizeLerpTarget);
             worldBorder.load(properties);
             this.vanillaWorldBorder = worldBorder;
         }
