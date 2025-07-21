45d2f5a168d5952120b4cefdc2825919d7ac43f8
sakurawald <sakurawald@gmail.com>
2025-07-21 16:31:58 +0800

    refactor: clean up for better_info module.

--- fabric/src/main/java/io/github/sakurawald/fuji/module/mixin/gameplay/carpet/better_info/InfoCommandMixin.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/mixin/gameplay/carpet/better_info/InfoCommandMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtHelper;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
@@ -18,29 +17,24 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(InfoCommand.class)
 public class InfoCommandMixin {
 
-    @SuppressWarnings("DataFlowIssue")
     @Inject(method = "infoBlock", at = @At(value = "INVOKE", target = "Lcarpet/commands/InfoCommand;printBlock(Ljava/util/List;Lnet/minecraft/server/command/ServerCommandSource;Ljava/lang/String;)V", shift = At.Shift.AFTER))
     private static void addNbtQueryForInfoBlockCommand(@NotNull ServerCommandSource source, @NotNull BlockPos pos, String grep, CallbackInfoReturnable<Integer> cir) {
-        // is player ?
-        ServerPlayerEntity player = source.getPlayer();
-        if (player == null) return;
+        MutableText additionalText = Text.empty().append("\n");
 
-        // is block entity ?
-        BlockEntity blockEntity = player.getWorld().getBlockEntity(pos);
-
-        MutableText output = Text.empty().append("\n");
+        BlockEntity blockEntity = source.getWorld().getBlockEntity(pos);
         if (blockEntity == null) {
-            player.sendMessage(output.append(Text.literal("No block entity found at " + pos.getX() + ", " + pos.getY() + ", " + pos.getZ())));
+            source.sendMessage(additionalText.append(Text.literal("No block entity found at " + pos.getX() + ", " + pos.getY() + ", " + pos.getZ())));
             return;
         }
 
-        // send nbt data
         #if MC_VER <= MC_1_20_4
         NbtCompound compoundTag = blockEntity.createNbtWithIdentifyingData();
         #elif MC_VER > MC_1_20_4
         NbtCompound compoundTag = blockEntity.createNbtWithIdentifyingData(blockEntity.getWorld().getRegistryManager());
         #endif
+        MutableText nbtDataText = Text.translatable("commands.data.block.query", pos.getX(), pos.getY(), pos.getZ(), NbtHelper.toPrettyPrintedText(compoundTag));
+        additionalText = additionalText.append(nbtDataText);
 
-        player.sendMessage(output.append(Text.translatable("commands.data.block.query", pos.getX(), pos.getY(), pos.getZ(), NbtHelper.toPrettyPrintedText(compoundTag))));
+        source.sendMessage(additionalText);
     }
 }
