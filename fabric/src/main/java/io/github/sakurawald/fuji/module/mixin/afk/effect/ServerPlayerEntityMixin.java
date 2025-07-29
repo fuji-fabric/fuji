a380e054e232da201e1c83fc5a409f19f3c5cc49
sakurawald <sakurawald@gmail.com>
2025-07-29 15:07:12 +0800

    fix: the injector priority for moveable option.

--- fabric/src/main/java/io/github/sakurawald/fuji/module/mixin/afk/effect/ServerPlayerEntityMixin.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/mixin/afk/effect/ServerPlayerEntityMixin.java
@@ -23,7 +23,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ServerPlayerEntity.class)
+// NOTE: Handle events later to ensure `moveable` option works.
+@Mixin(value = ServerPlayerEntity.class, priority = 1000 + 500)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
 
     @Unique
