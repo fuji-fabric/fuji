4e992a3ac0bfcee2481315cf524a2a137ebb5ec2
sakurawald <sakurawald@gmail.com>
2025-07-30 17:54:44 +0800

    refactor: make ListWorksGui extend the CrudPagedGui

--- fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/works/gui/ListWorksGui.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/works/gui/ListWorksGui.java
@@ -1,13 +1,13 @@
 package io.github.sakurawald.fuji.module.initializer.works.gui;
 
 import eu.pb4.sgui.api.elements.GuiElementBuilder;
-import eu.pb4.sgui.api.elements.GuiElementInterface;
 import eu.pb4.sgui.api.gui.SimpleGui;
 import io.github.sakurawald.fuji.core.auxiliary.minecraft.GuiHelper;
 import io.github.sakurawald.fuji.core.auxiliary.minecraft.PlayerHelper;
 import io.github.sakurawald.fuji.core.auxiliary.minecraft.RegistryHelper;
 import io.github.sakurawald.fuji.core.auxiliary.minecraft.ServerHelper;
 import io.github.sakurawald.fuji.core.auxiliary.minecraft.TextHelper;
+import io.github.sakurawald.fuji.core.gui.impl.gui.CrudPagedGui;
 import io.github.sakurawald.fuji.core.gui.impl.gui.PagedGui;
 import io.github.sakurawald.fuji.core.structure.GlobalPos;
 import io.github.sakurawald.fuji.module.initializer.works.WorksInitializer;
@@ -23,21 +23,12 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 
-public class ListWorksGui extends PagedGui<Work> {
+public class ListWorksGui extends CrudPagedGui<Work> {
 
     public ListWorksGui(ServerPlayerEntity player, @NotNull List<Work> entities, int pageIndex) {
         super(null, player, TextHelper.getTextByKey(player, "works.list.title"), entities, pageIndex);
 
         /* Place buttons in footer. */
-        getFooter().setSlot(3, GuiHelper
-            .makeAddButton(player)
-            .setName(TextHelper.getTextByKey(player, "works.list.add"))
-            .setCallback(() -> new CreateWorkGui(player).open())
-        );
-        getFooter().setSlot(4, GuiHelper
-            .makeHelpButton(player)
-            .setLore(TextHelper.getTextListByKey(player, "works.list.help.lore")));
-
         if (isViewingAllWorks(entities)) {
             getFooter().setSlot(5, GuiHelper
                 .makeLetterAButton(player)
@@ -62,6 +53,27 @@ public class ListWorksGui extends PagedGui<Work> {
         return entities == WorksInitializer.works.model().works;
     }
 
+    @Override
+    protected GuiElementBuilder toGuiElementBuilder(Work entity) {
+        ServerPlayerEntity player = getPlayer();
+
+        return new GuiElementBuilder()
+            .setItem(entity.getEntityIcon())
+            .setName(TextHelper.getTextByValue(null, entity.name))
+            .setLore(entity.ofLore(player));
+    }
+
+    @Override
+    protected @NotNull String getGuiHelpLoreKey() {
+        return "works.list.help.lore";
+    }
+
+    @Override
+    protected void onCreateEntity() {
+        new CreateWorkGui(this.getPlayer())
+            .open();
+    }
+
     @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     private boolean canOperateOnThisEntity(@NotNull ServerPlayerEntity player, @NotNull Work work) {
         return PlayerHelper.getPlayerName(player).equals(work.creator)
@@ -69,52 +81,61 @@ public class ListWorksGui extends PagedGui<Work> {
     }
 
     @Override
-    protected GuiElementInterface toGuiElement(@NotNull Work entity) {
-        ServerPlayerEntity player = getPlayer();
-
-        return new GuiElementBuilder()
-            .setItem(entity.getEntityIcon())
-            .setName(TextHelper.getTextByValue(null, entity.name))
-            .setLore(entity.ofLore(player))
-            .setCallback(handleClick(entity, player)).build();
+    protected boolean canCreateEntity() {
+        return true;
     }
 
-    private GuiElementInterface.ItemClickCallback handleClick(@NotNull Work entity, ServerPlayerEntity player) {
-        return (index, clickType, actionType) -> {
-            /* left click -> visit */
-            if (clickType.isLeft) {
-                RegistryKey<World> worldKey = RegistryKey.of(RegistryKeys.WORLD, RegistryHelper.makeIdentifier(entity.level));
-                ServerWorld level = ServerHelper.getServer().getWorld(worldKey);
-                if (level != null) {
-                    new GlobalPos(level, entity.x, entity.y, entity.z, entity.yaw, entity.pitch)
-                        .teleport(player);
-                } else {
-                    TextHelper.sendTextByKey(player, "world.dimension.not_found", entity.level);
-                }
+    @Override
+    protected boolean canReadEntity(Work entity) {
+        return true;
+    }
 
-                this.close();
-                return;
-            }
-            /* shift + right click -> specialized settings */
-            if (clickType.isRight && clickType.shift) {
-                if (!canOperateOnThisEntity(player, entity)) {
-                    TextHelper.sendTextByKey(player, "works.work.set.no_perm");
-                    return;
-                }
-                entity.openSpecializedSettingsGui(player, gui);
-                this.close();
-                return;
-            }
-            /* right click -> general settings */
-            if (clickType.isRight) {
-                if (!canOperateOnThisEntity(player, entity)) {
-                    TextHelper.sendTextByKey(player, "works.work.set.no_perm");
-                    return;
-                }
-                entity.openGeneralSettingsGui(player, gui);
-                this.close();
-            }
-        };
+    @Override
+    protected boolean canUpdateEntity(Work entity) {
+        return canOperateOnThisEntity(getPlayer(), entity);
+    }
+
+    @Override
+    protected boolean canDeleteEntity(Work entity) {
+        return canOperateOnThisEntity(getPlayer(), entity);
+    }
+
+    @Override
+    protected void onLeftClickEntity(Work entity) {
+        RegistryKey<World> worldKey = RegistryKey.of(RegistryKeys.WORLD, RegistryHelper.makeIdentifier(entity.level));
+        ServerPlayerEntity player = getPlayer();
+        ServerWorld dimension = ServerHelper.getServer().getWorld(worldKey);
+
+        if (dimension != null) {
+            new GlobalPos(dimension, entity.x, entity.y, entity.z, entity.yaw, entity.pitch)
+                .teleport(player);
+        } else {
+            TextHelper.sendTextByKey(player, "world.dimension.not_found", entity.level);
+        }
+
+        this.close();
+    }
+
+    @Override
+    protected void onRightShiftClickEntity(Work entity) {
+        ServerPlayerEntity player = getPlayer();
+        if (!canUpdateEntity(entity)) {
+            TextHelper.sendTextByKey(player, "works.work.set.no_perm");
+            return;
+        }
+        entity.openSpecializedSettingsGui(player, gui);
+        this.close();
+    }
+
+    @Override
+    protected void onRightClickEntity(Work entity) {
+        ServerPlayerEntity player = getPlayer();
+        if (!canUpdateEntity(entity)) {
+            TextHelper.sendTextByKey(player, "works.work.set.no_perm");
+            return;
+        }
+        entity.openGeneralSettingsGui(player, gui);
+        this.close();
     }
 
 }
