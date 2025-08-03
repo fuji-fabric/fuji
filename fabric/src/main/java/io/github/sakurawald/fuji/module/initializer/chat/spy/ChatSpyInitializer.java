5b75ede65b33087bb37b436cea516890cb9057c9
sakurawald <sakurawald@gmail.com>
2025-08-03 19:08:58 +0800

    refactor: split the config model and data model for chat.spy module.

--- fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/chat/spy/ChatSpyInitializer.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/chat/spy/ChatSpyInitializer.java
@@ -13,11 +13,14 @@ import io.github.sakurawald.fuji.core.config.handler.abst.BaseConfigurationHandl
 import io.github.sakurawald.fuji.core.config.handler.impl.ObjectConfigurationHandler;
 import io.github.sakurawald.fuji.module.initializer.ModuleInitializer;
 import io.github.sakurawald.fuji.module.initializer.chat.spy.config.model.ChatSpyConfigModel;
+import io.github.sakurawald.fuji.module.initializer.chat.spy.config.model.ChatSpyDataModel;
+import java.util.function.Function;
 import net.minecraft.network.message.MessageType;
 import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.NotNull;
 
 @Document(id = 1751826708198L, value = """
     This module allows you to spy on specified chat message type. (Mainly used for private message types)
@@ -38,33 +41,35 @@ public class ChatSpyInitializer extends ModuleInitializer {
 
     private static String lastContentString = "";
     public static final BaseConfigurationHandler<ChatSpyConfigModel> config = new ObjectConfigurationHandler<>(BaseConfigurationHandler.CONFIG_JSON, ChatSpyConfigModel.class);
+    public static final BaseConfigurationHandler<ChatSpyDataModel> data = new ObjectConfigurationHandler<>("data.json", ChatSpyDataModel.class);
 
     @Document(id = 1751826711342L, value = "Enable/disable the chat spy mode for you.")
     @CommandNode("toggle")
     private static int $toggle(@CommandSource ServerPlayerEntity player) {
-        ChatSpyConfigModel.PerPlayerOptions options = withOptions(player);
-        options.enabled = !options.enabled;
-        config.writeStorage();
-
-        TextHelper.sendTextByKey(player, options.enabled ? "on" : "off");
+        withPlayerOptions(player, true, (playerOptions) -> {
+            playerOptions.setEnabled(!playerOptions.isEnabled());
+            TextHelper.sendTextByKey(player, playerOptions.isEnabled() ? "on" : "off");
+            return null;
+        });
         return CommandHelper.Return.SUCCESS;
     }
 
-    private static void ensureOptionsExists(ServerPlayerEntity player) {
-        String key = player.getGameProfile().getName();
-        config.model().getOptions().putIfAbsent(key, new ChatSpyConfigModel.PerPlayerOptions());
+    private static <T> T withPlayerOptions(@NotNull ServerPlayerEntity player, boolean writeStorage, @NotNull Function<ChatSpyDataModel.PerPlayerOptions, T> function) {
+        String key = PlayerHelper.getPlayerName(player);
+        var playerOptions = data.model().getOptions().computeIfAbsent(key, k -> new ChatSpyDataModel.PerPlayerOptions());
+
+        T apply = function.apply(playerOptions);
+        if (writeStorage) {
+            data.writeStorage();
+        }
+        return apply;
     }
 
-    public static ChatSpyConfigModel.PerPlayerOptions withOptions(ServerPlayerEntity player) {
-        ensureOptionsExists(player);
-        return config.model().getOptions().get(player.getGameProfile().getName());
-    }
-
-    public static void processChatSpy(String messageTypeString, ServerPlayerEntity receiverPlayer, SignedMessage signedMessage, MessageType.Parameters parameters) {
+    public static void processChatSpy(@NotNull String messageTypeString, @NotNull ServerPlayerEntity receiverPlayer, @NotNull SignedMessage signedMessage, @NotNull MessageType.Parameters parameters) {
         LogUtil.debug("Receive a message with message type {}", messageTypeString);
 
         /* Filter for whitelisted message types.  */
-        if (config.model().message_type.whitelist
+        if (config.model().getMessageType().getAcceptors()
             .stream()
             .noneMatch(it -> it.matches(messageTypeString))) {
             return;
@@ -76,7 +81,7 @@ public class ChatSpyInitializer extends ModuleInitializer {
 
         /* Filter for duplicated chat string. */
         // NOTE: The sent message will be sent to all online players.
-        if (config.model().ignore_consecutive_same_text && contentString.equals(lastContentString)) {
+        if (config.model().isIgnoreConsecutiveSameText() && contentString.equals(lastContentString)) {
             return;
         }
         lastContentString = contentString;
@@ -90,14 +95,14 @@ public class ChatSpyInitializer extends ModuleInitializer {
             .append(receiverPlayerName);
 
         /* Log the console. */
-        if (config.model().log_console) {
+        if (config.model().isLogConsole()) {
             LogUtil.info(notificationText.getString());
         }
 
         /* Send the notification. */
         PlayerHelper.Lookup.getOnlinePlayers()
             .stream()
-            .filter(it -> withOptions(it).enabled)
+            .filter(it -> withPlayerOptions(it, false, ChatSpyDataModel.PerPlayerOptions::isEnabled))
             .forEach(it -> it.sendMessage(notificationText));
     }
 }
--- fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/chat/spy/config/model/ChatSpyConfigModel.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/chat/spy/config/model/ChatSpyConfigModel.java
@@ -1,11 +1,11 @@
 package io.github.sakurawald.fuji.module.initializer.chat.spy.config.model;
 
+import com.google.gson.annotations.SerializedName;
 import io.github.sakurawald.fuji.core.document.annotation.Document;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @Data
@@ -15,10 +15,14 @@ public class ChatSpyConfigModel {
     @Document(id = 1751826718752L, value = """
         Only accept and spy on `messages` whose `message type` meets the `whitelist`.
         """)
-    public MessageType message_type = new MessageType();
+    MessageType messageType = new MessageType();
+
+    @Data
+    @NoArgsConstructor
     public static class MessageType {
 
-        public List<String> whitelist = new ArrayList<>() {
+        @SerializedName(value = "acceptors", alternate = "whitelist")
+        List<String> acceptors = new ArrayList<>() {
             {
                 this.add("minecraft:msg_command_incoming");
             }
@@ -29,25 +33,11 @@ public class ChatSpyConfigModel {
     @Document(id = 1751826720527L, value = """
         Should not spy on `consecutive same text`.
         """)
-    public boolean ignore_consecutive_same_text = true;
+    boolean ignoreConsecutiveSameText = true;
 
     @Document(id = 1751826722259L, value = """
         Should we also log the `console` what is spied?
         """)
-    public boolean log_console = false;
-
-    @Document(id = 1751826724306L, value = """
-        Saved per-player options.
-        """)
-    public final HashMap<String, PerPlayerOptions> options = new HashMap<>();
-
-    @Data
-    @NoArgsConstructor
-    public static class PerPlayerOptions {
-        @Document(id = 1751826726521L, value = """
-            Is `chat spy` mode enabled?
-            """)
-        public boolean enabled = false;
-    }
+    boolean logConsole = false;
 
 }
