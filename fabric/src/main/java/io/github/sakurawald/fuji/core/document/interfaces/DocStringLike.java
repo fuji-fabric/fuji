21e15d87e830fd2a0d1ad881fd7749e81a8e4638
sakurawald <sakurawald@gmail.com>
2025-08-09 14:18:52 +0800

    refactor: implements the DocStringLike interface

--- fabric/src/main/java/io/github/sakurawald/fuji/core/document/interfaces/DocStringLike.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/document/interfaces/DocStringLike.java
@@ -1,6 +1,6 @@
 package io.github.sakurawald.fuji.core.document.interfaces;
 
 public interface DocStringLike {
-    long id();
-    String value();
+    long getId();
+    String getValue();
 }
--- fabric/src/main/java/io/github/sakurawald/fuji/core/document/structure/DocString.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/document/structure/DocString.java
@@ -1,15 +1,18 @@
 package io.github.sakurawald.fuji.core.document.structure;
 
+import io.github.sakurawald.fuji.core.document.interfaces.DocStringLike;
 import lombok.Data;
+import org.jetbrains.annotations.NotNull;
 
+@SuppressWarnings("ClassCanBeRecord")
 @Data
-public class DocString {
+public class DocString implements DocStringLike {
     public static final String DOC_STRING_KEY_PREFIX = "docstring.";
 
-    private final long id;
-    private final String value;
+    final long id;
+    final String value;
 
-    public static long parseDocStringId(String jsonKey) {
+    public static long parseDocStringId(@NotNull String jsonKey) {
         String substring = jsonKey.substring(DOC_STRING_KEY_PREFIX.length());
         return Long.parseLong(substring);
     }
