d1c6e7ae95b4cec7f81f4efe17a047e86307e3ee
sakurawald <sakurawald@gmail.com>
2025-08-10 14:48:20 +0800

    fix: try pass CI

--- fabric/src/main/java/io/github/sakurawald/fuji/core/command/argument/adapter/impl/StyleArgumentTypeAdapter.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/command/argument/adapter/impl/StyleArgumentTypeAdapter.java
@@ -39,7 +39,7 @@ public class StyleArgumentTypeAdapter extends BaseArgumentTypeAdapter {
     @Override
     protected Object makeArgumentValue(@NotNull CommandContext<ServerCommandSource> context, @NotNull CommandArgument commandArgument) {
         #if MC_VER <= MC_1_20_2
-        return new GreedyString(StringArgumentType.getString(context, argument.getArgumentName()));
+        return new GreedyString(StringArgumentType.getString(context, commandArgument.getArgumentName()));
         #elif MC_VER > MC_1_20_2
         return StyleArgumentType.getStyle(context, commandArgument.getArgumentName());
         #endif
