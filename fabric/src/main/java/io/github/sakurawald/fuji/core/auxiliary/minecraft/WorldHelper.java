332220b7de55761fa60f904a0fe1f7643f551137
sakurawald <sakurawald@gmail.com>
2025-08-19 11:06:42 +0800

    feature: add raycast helper functions.

--- fabric/src/main/java/io/github/sakurawald/fuji/core/auxiliary/minecraft/WorldHelper.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/auxiliary/minecraft/WorldHelper.java
@@ -2,14 +2,18 @@ package io.github.sakurawald.fuji.core.auxiliary.minecraft;
 
 import java.util.Collection;
 import java.util.Optional;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.projectile.ProjectileUtil;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ChunkHolder;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
@@ -103,8 +107,10 @@ public class WorldHelper {
 
     public static class Raycast {
 
+        private static final double PLAYER_INTERACTION_DISTANCE = 5.0;
+
         public static Optional<BlockPos> getLookingAtBlock(@NotNull ServerPlayerEntity player) {
-            return getLookingAtBlock(player, 5.0);
+            return getLookingAtBlock(player, PLAYER_INTERACTION_DISTANCE);
         }
 
         public static Optional<BlockPos> getLookingAtBlock(@NotNull ServerPlayerEntity player, double maxDistance) {
@@ -113,11 +119,11 @@ public class WorldHelper {
             Vec3d reachVec = eyePos.add(lookVec.multiply(maxDistance));
 
             BlockHitResult blockHitResult = player.getWorld().raycast(new RaycastContext(
-                    eyePos,
-                    reachVec,
-                    RaycastContext.ShapeType.OUTLINE,
-                    RaycastContext.FluidHandling.NONE,
-                    player
+                eyePos,
+                reachVec,
+                RaycastContext.ShapeType.OUTLINE,
+                RaycastContext.FluidHandling.NONE,
+                player
             ));
 
             if (blockHitResult.getType() == HitResult.Type.BLOCK) {
@@ -125,6 +131,33 @@ public class WorldHelper {
             }
 
             return Optional.empty();
+        }
+
+        public static Optional<Entity> getLookingAtEntity(@NotNull ServerPlayerEntity player) {
+            return getLookingAtEntity(player, PLAYER_INTERACTION_DISTANCE);
+        }
+
+        public static Optional<Entity> getLookingAtEntity(@NotNull ServerPlayerEntity player, double maxDistance) {
+            Vec3d start = player.getCameraPosVec(1.0F);
+            Vec3d direction = player.getRotationVec(1.0F);
+            Vec3d end = start.add(direction.multiply(maxDistance));
+
+            Box box = player.getBoundingBox().stretch(direction.multiply(maxDistance)).expand(1.0D, 1.0D, 1.0D);
+            EntityHitResult entityHitResult = ProjectileUtil.raycast(
+                player,
+                start,
+                end,
+                box,
+                entity -> !entity.isSpectator() && entity.isAttackable() && entity != player,
+                maxDistance * maxDistance
+            );
+
+            if (entityHitResult != null) {
+                return Optional.ofNullable(entityHitResult.getEntity());
+            }
+
+            return Optional.empty();
+
         }
     }
 
