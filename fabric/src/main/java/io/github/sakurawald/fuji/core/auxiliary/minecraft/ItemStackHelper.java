e6a7d2bae0552a306972bfdcfe1a24b651a23745
sakurawald <sakurawald@gmail.com>
2025-08-20 20:06:48 +0800

    fix: find the version range for ItemStringReader

--- fabric/src/main/java/io/github/sakurawald/fuji/core/auxiliary/minecraft/ItemStackHelper.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/auxiliary/minecraft/ItemStackHelper.java
@@ -110,10 +110,10 @@ public class ItemStackHelper {
             try {
                 ItemStack stack;
 
-                #if MC_VER < MC_1_21_4
+                #if MC_VER < MC_1_21_6
                 ItemStackArgument itemStackArgument = ItemStackArgumentType.itemStack(CommandHelper.getCommandRegistryAccess()).parse(stringReader);
                 stack = itemStackArgument.createStack(1, false);
-                #elif MC_VER >= MC_1_21_4
+                #elif MC_VER >= MC_1_21_6
                 var itemStringParser = new ItemStringReader(CommandHelper.getCommandRegistryAccess());
                 ItemStringReader.ItemResult consume = itemStringParser.consume(stringReader);
                 ItemStackArgument itemStackArgument = new ItemStackArgument(consume.comp_628(), consume.comp_2439());
