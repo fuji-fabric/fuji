c87c392a69bb9c021a75cdd7a96ac868e5193ec6
sakurawald <sakurawald@gmail.com>
2025-08-26 18:49:42 +0800

    fix: the `ON_EXECUTION_{SUCCESS|FAILURE}` advice type didn't get triggered in MC [1.20, 1.20.2]

--- fabric/src/main/java/io/github/sakurawald/fuji/module/mixin/command_advice/CommandDispatcherMixin.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/mixin/command_advice/CommandDispatcherMixin.java
@@ -1,11 +1,15 @@
 package io.github.sakurawald.fuji.module.mixin.command_advice;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.ParseResults;
 import io.github.sakurawald.fuji.core.auxiliary.minecraft.CommandHelper;
 import io.github.sakurawald.fuji.module.initializer.command_advice.CommandAdviceInitializer;
 import io.github.sakurawald.fuji.module.initializer.command_advice.structure.CommandAdviceType;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import net.minecraft.server.command.ServerCommandSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -26,13 +30,27 @@ public class CommandDispatcherMixin {
     }
 
     #if MC_VER <= MC_1_20_2
-    @Inject(method = "execute(Lcom/mojang/brigadier/ParseResults;)I", at = @At(value = "INVOKE", target = "Lcom/mojang/brigadier/Command;run(Lcom/mojang/brigadier/context/CommandContext;)I", shift = At.Shift.AFTER), cancellable = true)
+    @WrapOperation(method = "execute(Lcom/mojang/brigadier/ParseResults;)I", at = @At(value = "INVOKE", target = "Lcom/mojang/brigadier/Command;run(Lcom/mojang/brigadier/context/CommandContext;)I"))
+    int afterExecuteInCommandDispatcher(Command<ServerCommandSource> instance, CommandContext<ServerCommandSource> commandContext, Operation<Integer> original, @Local(argsOnly = true) ParseResults<ServerCommandSource> parseResults)
     #elif MC_VER > MC_1_20_2
-    @Inject(method = "execute(Lcom/mojang/brigadier/ParseResults;)I", at = @At(value = "INVOKE", target = "Lcom/mojang/brigadier/context/ContextChain;executeAll(Ljava/lang/Object;Lcom/mojang/brigadier/ResultConsumer;)I", shift = At.Shift.AFTER))
+    @WrapMethod(method = "execute(Lcom/mojang/brigadier/ParseResults;)I")
     #endif
-    void afterExecuteInCommandDispatcher(ParseResults<?> parseResults, CallbackInfoReturnable<Integer> cir) {
+    int afterExecuteInCommandDispatcher(ParseResults<ServerCommandSource> parseResults, Operation<Integer> original)
+    {
+        AtomicInteger logicalReturnValue = new AtomicInteger(0);
         CommandHelper.Source.withServerCommandSource(parseResults.getContext(), (serverCommandSource)  -> {
-            CommandAdviceInitializer.processCommandAdvice(this, serverCommandSource, parseResults.getReader().getString(), CommandAdviceType.AFTER_EXECUTION, Optional.of(cir), Optional.ofNullable(cir.getReturnValue()));
+
+            #if MC_VER <= MC_1_20_2
+            logicalReturnValue.set(original.call(instance, commandContext));
+            boolean logicalSuccess = CommandHelper.Return.isSuccess(logicalReturnValue);
+            CommandAdviceType adviceType = logicalSuccess ? CommandAdviceType.ON_EXECUTION_SUCCESS : CommandAdviceType.ON_EXECUTION_FAILURE;
+            CommandAdviceInitializer.processCommandAdvice(this, serverCommandSource, parseResults.getReader().getString(), adviceType, Optional.empty(), Optional.of(logicalReturnValue));
+            #elif MC_VER > MC_1_20_2
+            logicalReturnValue.set(original.call(parseResults));
+            #endif
+
+            CommandAdviceInitializer.processCommandAdvice(this, serverCommandSource, parseResults.getReader().getString(), CommandAdviceType.AFTER_EXECUTION, Optional.empty(), Optional.of(logicalReturnValue.get()));
         });
+        return logicalReturnValue.get();
     }
 }
