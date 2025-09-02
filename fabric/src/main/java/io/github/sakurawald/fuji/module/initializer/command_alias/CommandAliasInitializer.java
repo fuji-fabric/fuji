e2cf7255781d0d61c9647b5dbc4400618ed0b5e9
sakurawald <sakurawald@gmail.com>
2025-09-02 13:37:17 +0800

    refactor: extract command alias service.

--- fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/command_alias/CommandAliasInitializer.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/command_alias/CommandAliasInitializer.java
@@ -1,16 +1,9 @@
 package io.github.sakurawald.fuji.module.initializer.command_alias;
 
-import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
-import io.github.sakurawald.fuji.core.auxiliary.LogUtil;
-import io.github.sakurawald.fuji.core.auxiliary.minecraft.CommandHelper;
 import io.github.sakurawald.fuji.core.command.annotation.CommandNode;
 import io.github.sakurawald.fuji.core.command.annotation.CommandRequirement;
 import io.github.sakurawald.fuji.core.command.annotation.CommandSource;
-import io.github.sakurawald.fuji.core.command.argument.structure.CommandArgument;
-import io.github.sakurawald.fuji.core.command.descriptor.CommandDescriptor;
-import io.github.sakurawald.fuji.core.command.processor.CommandAnnotationProcessor;
-import io.github.sakurawald.fuji.core.command.structure.CommandRequirementDescriptor;
 import io.github.sakurawald.fuji.core.config.handler.abst.BaseConfigurationHandler;
 import io.github.sakurawald.fuji.core.config.handler.impl.ObjectConfigurationHandler;
 import io.github.sakurawald.fuji.core.document.annotation.ColorBox;
@@ -20,12 +13,9 @@ import io.github.sakurawald.fuji.core.event.impl.CommandEvents;
 import io.github.sakurawald.fuji.core.event.impl.ServerLifecycleEvents;
 import io.github.sakurawald.fuji.module.initializer.ModuleInitializer;
 import io.github.sakurawald.fuji.module.initializer.command_alias.config.model.CommandAliasConfigModel;
+import io.github.sakurawald.fuji.module.initializer.command_alias.service.CommandAliasService;
 import io.github.sakurawald.fuji.module.initializer.command_alias.structure.AliasCommandDescriptor;
-import io.github.sakurawald.fuji.module.initializer.command_alias.structure.CommandAliasEntry;
-import java.util.List;
-import java.util.Optional;
 import net.minecraft.server.command.ServerCommandSource;
-import org.jetbrains.annotations.NotNull;
 
 @Document(id = 1751826302190L, value = """
     This module allows you to define alias for existing commands.
@@ -51,82 +41,27 @@ import org.jetbrains.annotations.NotNull;
 @CommandRequirement(level = 4)
 public class CommandAliasInitializer extends ModuleInitializer {
 
-    private static final BaseConfigurationHandler<CommandAliasConfigModel> config = ObjectConfigurationHandler.ofModule(BaseConfigurationHandler.CONFIG_JSON_LITERAL, CommandAliasConfigModel.class);
+    public static final BaseConfigurationHandler<CommandAliasConfigModel> config = ObjectConfigurationHandler.ofModule(BaseConfigurationHandler.CONFIG_JSON_LITERAL, CommandAliasConfigModel.class);
 
     @Document(id = 1756022056042L, value = "List all registered alias-commands in server.")
     @CommandNode("list")
     private static int $list(@CommandSource CommandContext<ServerCommandSource> ctx) {
-        return CommandsInspectionGui.inspectCommandDescriptors(ctx, it -> it instanceof AliasCommandDescriptor);
+        return CommandsInspectionGui
+            .inspectCommandDescriptors(ctx, it -> it instanceof AliasCommandDescriptor);
     }
 
     @Override
     protected void onInitialize() {
         ServerLifecycleEvents.SERVER_STARTED.register((server) -> {
-            registerAllAliasCommands();
-            CommandEvents.REGISTRATION.register((a, b, c) -> registerAllAliasCommands());
+            CommandAliasService.registerAllAliasCommands();
+            CommandEvents.REGISTRATION.register((a, b, c) -> CommandAliasService.registerAllAliasCommands());
         });
     }
 
     @Override
     protected void onReload() {
-        unregisterAllAliasCommands();
-        registerAllAliasCommands();
-    }
-
-    private static void registerAllAliasCommands() {
-        config.model()
-            .getAliases()
-            .stream()
-            .filter(CommandAliasEntry::isEnable)
-            .map(CommandAliasInitializer::makeRedirectCommandDescriptor)
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-            .forEach(it -> {
-                LogUtil.info("Register alias command: {}", it.getCommandSyntax());
-                it.register();
-            });
-        CommandHelper.updateCommandTree();
-    }
-
-    private static void unregisterAllAliasCommands() {
-        List<CommandDescriptor> registeredCommandDescriptors = getRegisteredAliasCommandDescriptors();
-
-        LogUtil.info("Un-register alias commands.");
-        registeredCommandDescriptors
-            .forEach(it -> {
-                LogUtil.info("Un-register alias command: {}", it.getCommandSyntax());
-                it.unregister();
-            });
-        CommandHelper.updateCommandTree();
-    }
-
-    private static List<CommandDescriptor> getRegisteredAliasCommandDescriptors() {
-        return CommandAnnotationProcessor.REGISTERED_COMMAND_DESCRIPTORS
-            .stream()
-            .filter(it -> it instanceof AliasCommandDescriptor)
-            .toList();
-    }
-
-    private static Optional<AliasCommandDescriptor> makeRedirectCommandDescriptor(@NotNull CommandAliasEntry entry) {
-        /* Find the redirect target command node in server command tree. */
-        CommandDispatcher<ServerCommandSource> dispatcher = CommandHelper.getCommandDispatcher();
-        return Optional.ofNullable(dispatcher.findNode(entry.getTo()))
-            .map(redirectTargetCommandNode -> {
-                CommandRequirementDescriptor requirement = entry.getRequirement();
-                List<CommandArgument> commandArguments = entry.getFrom()
-                    .stream()
-                    .map(argumentName -> CommandArgument.ofLiteralArgument(argumentName, requirement))
-                    .toList();
-
-                AliasCommandDescriptor descriptor = new AliasCommandDescriptor(commandArguments, redirectTargetCommandNode);
-                descriptor.fillDocument(entry.getDocument());
-
-                return Optional.of(descriptor);
-            })
-            .orElseGet(() -> {
-                LogUtil.warn("Failed to find the target command node {} in server command tree. Ignoring the command alias entry: {}", entry.getTo(), entry);
-                return Optional.empty();
-            });
+        CommandAliasService.unregisterAllAliasCommands();
+        CommandAliasService.registerAllAliasCommands();
     }
 
 }
