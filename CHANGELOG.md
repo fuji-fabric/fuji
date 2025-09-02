5fa1232122b221f23dcd52edae4f5108c10bf463
sakurawald <sakurawald@gmail.com>
2025-09-02 17:48:59 +0800

    docs: document the methods in ExceptionUtil

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,9 +6,3 @@
 
 # Changelog
 
-- [cleaner] Enhanced the `cleaner` module.
-  - **Documentation**: Added comprehensive documentation for this module.
-  - **Feature**: Introduced support for specifying the `cleanup method` (`KILL` or `DISCARD`).
-  - **Feature**: Added a new `enable` property for each `entity matcher` to simplify configuration.
-  - **Feature**: Added support for targeting both `block-attached entities` and `vehicle entities`.
-  - **Feature**: Cleanup reports now broadcast the `actual count` of cleaned `item entities`.  
--- fabric/src/main/java/io/github/sakurawald/fuji/core/auxiliary/ExceptionUtil.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/auxiliary/ExceptionUtil.java
@@ -1,6 +1,7 @@
 package io.github.sakurawald.fuji.core.auxiliary;
 
 import com.google.errorprone.annotations.CheckReturnValue;
+import io.github.sakurawald.fuji.core.document.annotation.ForDeveloper;
 import io.github.sakurawald.fuji.core.document.annotation.TestCase;
 import io.github.sakurawald.fuji.core.exception.FujiException;
 import org.jetbrains.annotations.NotNull;
@@ -14,6 +15,22 @@ public class ExceptionUtil {
         "This mod failed at the execution of `/json read a b` command.",
         "This mod failed at the execution of `/run as console run as player %player:name% bad` command."
     })
+    @ForDeveloper("""
+        Call this method, if you need to express the `TRY-CATCH-WRAP-RETHROW` pattern.
+
+        Unfortunately, the Java language doesn't support to throw a checked exception, without the `throws` signature.
+        It introduce the in-convenience especially inside the functional interfaces.
+
+        One solution is to use manifold-exceptions, and treat the checked-exception as unchecked-exception.
+        This solution requires the manifold-rt, to install the host at JVM runtime.
+        It works well in fabric platform.
+        However, it breaks the compatibility with `connector` mod's `class loader (TRANSFORMER)`.
+
+        You will get a big ClassNotFoundException, due to the class is loaded via different loaders.
+        The manifold-rt needs to generate the byte-code at runtime, to provide the power.
+
+        Finally, use the vanilla Java's pattern, for better compatibility.
+        """)
     @CheckReturnValue
     public static @NotNull RuntimeException makeReThrownException(@NotNull Exception exception) {
         return ExceptionUtil.makeReThrownException("⬆⬆⬆⬆⬆ Re-throw exception by Fuji Mod. Refer to the details above. ⬆⬆⬆⬆⬆", exception);
