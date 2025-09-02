6b33a1616fac2a2295b7b8363f412842d21cbf55
sakurawald <sakurawald@gmail.com>
2025-09-02 12:44:07 +0800

    refactor: extract entity helper functions

--- fabric/src/main/java/io/github/sakurawald/fuji/core/auxiliary/minecraft/EntityHelper.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/auxiliary/minecraft/EntityHelper.java
@@ -1,7 +1,9 @@
 package io.github.sakurawald.fuji.core.auxiliary.minecraft;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
@@ -25,28 +27,11 @@ public class EntityHelper {
         return entity.getServer();
     }
 
-    public static boolean isBlockAttachedEntity(Entity entity) {
-        #if MC_VER <= MC_1_20_6
-        return entity instanceof net.minecraft.entity.decoration.LeashKnotEntity;
-        #elif MC_VER > MC_1_20_6
-        return entity instanceof net.minecraft.entity.decoration.BlockAttachedEntity;
-        #endif
-    }
-
     public static boolean isLeashed(Entity entity) {
         #if MC_VER <= MC_1_20_6
         return (entity instanceof net.minecraft.entity.mob.MobEntity mobEntity) && mobEntity.isLeashed();
         #elif MC_VER > MC_1_20_6
         return (entity instanceof net.minecraft.entity.Leashable leashable) && leashable.isLeashed();
-        #endif
-    }
-
-    public static boolean isVehicleEntity(Entity entity) {
-        #if MC_VER <= MC_1_20_2
-        return (entity instanceof net.minecraft.entity.vehicle.BoatEntity)
-        || (entity instanceof net.minecraft.entity.vehicle.AbstractMinecartEntity);
-        #elif MC_VER > MC_1_20_2
-        return entity instanceof net.minecraft.entity.vehicle.VehicleEntity;
         #endif
     }
 
@@ -68,5 +53,45 @@ public class EntityHelper {
     public static void updateVelocity(@NotNull Entity entity) {
         EntityVelocityUpdateS2CPacket packet = new EntityVelocityUpdateS2CPacket(entity);
         PacketHelper.sendPacketToAll(packet);
+    }
+
+    public static @NotNull String toTranslatableKey(@NotNull Entity entity) {
+        String translatableKey;
+
+        if (entity instanceof ItemEntity itemEntity) {
+            translatableKey = itemEntity.getStack().getItem().getTranslationKey();
+        } else {
+            translatableKey = entity.getType().getTranslationKey();
+        }
+        return translatableKey;
+    }
+
+    public static boolean hasCustomName(@NotNull Entity entity) {
+        if (entity instanceof ItemEntity itemEntity) {
+            ItemStack itemStack = itemEntity.getStack();
+            return ItemStackHelper.CustomName.hasCustomName(itemStack);
+        } else {
+            return entity.hasCustomName();
+        }
+    }
+
+    public static boolean isLivingEntity(@NotNull Entity entity) {
+        return entity.isLiving();
+    }
+
+    public static boolean hasVehicle(@NotNull Entity entity) {
+        return entity.hasVehicle();
+    }
+
+    public static boolean hasPassengers(@NotNull Entity entity) {
+        return entity.hasPassengers();
+    }
+
+    public static boolean isItemEntity(@NotNull Entity entity) {
+        return entity instanceof ItemEntity;
+    }
+
+    public static boolean isGlowing(@NotNull Entity entity) {
+        return entity.isGlowing();
     }
 }
