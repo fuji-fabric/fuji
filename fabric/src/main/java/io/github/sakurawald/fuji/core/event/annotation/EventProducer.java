fae3e027db879d688b57eacc22b67957fafe0010
sakurawald <sakurawald@gmail.com>
2025-09-05 22:00:25 +0800

    feature: explicitly specify the retention for @EventConsumer and @EventProducer

--- fabric/src/main/java/io/github/sakurawald/fuji/core/event/annotation/EventProducer.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/event/annotation/EventProducer.java
@@ -2,7 +2,13 @@ package io.github.sakurawald.fuji.core.event.annotation;
 
 import com.google.errorprone.annotations.Keep;
 import io.github.sakurawald.fuji.core.event.abst.BaseEvent;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.METHOD)
 @Keep
 public @interface EventProducer {
 
