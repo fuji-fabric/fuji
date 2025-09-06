ec94138e881ace7cd05561e255290699519c5330
sakurawald <sakurawald@gmail.com>
2025-09-06 15:27:58 +0800

    refactor: remove the event producer in old event system.

--- fabric/src/main/java/io/github/sakurawald/fuji/core/event/message/impl/ServerLifecycleEvents.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/event/message/impl/ServerLifecycleEvents.java
@@ -5,25 +5,17 @@ import net.minecraft.server.MinecraftServer;
 
 public class ServerLifecycleEvents {
 
-    public static SimpleEvent<ServerStartingCallback> SERVER_STARTING = new SimpleEvent<>((listeners) -> (server) -> listeners.forEach(listener -> listener.fire(server)));
 
     public static SimpleEvent<ServerStartedCallback> SERVER_STARTED = new SimpleEvent<>((listeners) -> (server) -> listeners.forEach(listener -> listener.fire(server)));
 
-    public static SimpleEvent<ServerStoppedCallback> SERVER_STOPPED = new SimpleEvent<>((listeners) -> (server) -> listeners.forEach(listener -> listener.fire(server)));
 
     public static SimpleEvent<ServerStoppingCallback> SERVER_STOPPING = new SimpleEvent<>((listeners) -> (server) -> listeners.forEach(listener -> listener.fire(server)));
 
-    public interface ServerStartingCallback {
-        void fire(MinecraftServer server);
-    }
 
     public interface ServerStartedCallback {
         void fire(MinecraftServer server);
     }
 
-    public interface ServerStoppedCallback {
-        void fire(MinecraftServer server);
-    }
 
     public interface ServerStoppingCallback {
         void fire(MinecraftServer server);
