3053f6f0b6a7d4125e65100f6e43bbcd1f1e2341
sakurawald <sakurawald@gmail.com>
2025-09-07 00:10:27 +0800

    feature: cache the dimensionId for world.border

--- fabric/src/main/java/io/github/sakurawald/fuji/module/mixin/world/border/WorldMixin.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/mixin/world/border/WorldMixin.java
@@ -1,21 +1,29 @@
 package io.github.sakurawald.fuji.module.mixin.world.border;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import io.github.sakurawald.fuji.core.annotation.HotPath;
 import io.github.sakurawald.fuji.core.auxiliary.minecraft.RegistryHelper;
 import io.github.sakurawald.fuji.module.initializer.world.border.WorldBorderInitializer;
 import io.github.sakurawald.fuji.module.initializer.world.border.structure.BorderDescriptor;
 import net.minecraft.world.World;
 import net.minecraft.world.border.WorldBorder;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(World.class)
 public class WorldMixin {
 
+    @Unique
+    @HotPath
+    private String dimensionId;
+
     @ModifyReturnValue(method = "getWorldBorder", at = @At("RETURN"))
     WorldBorder modifyTheReturnValueOfGetWorldBorder(WorldBorder original) {
-        World world = (World) (Object) this;
-        String dimensionId = RegistryHelper.getIdAsString(world);
+        if (dimensionId == null) {
+            World world = (World) (Object) this;
+            dimensionId = RegistryHelper.getIdAsString(world);
+        }
 
         return WorldBorderInitializer
             .getEffectiveBorderDescriptor(dimensionId)
