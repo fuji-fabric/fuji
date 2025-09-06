32fb3bf79e6a2f7b60c3e96564fc306c3a1020ec
sakurawald <sakurawald@gmail.com>
2025-09-06 15:49:31 +0800

    feature: allow to define multiple event producers in one mixin class.

--- fabric/src/main/java/io/github/sakurawald/fuji/core/event/inject/structure/EventGraph.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/event/inject/structure/EventGraph.java
@@ -1,9 +1,6 @@
 package io.github.sakurawald.fuji.core.event.inject.structure;
 
-import io.github.sakurawald.fuji.core.auxiliary.LogUtil;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
@@ -20,29 +17,21 @@ public class EventGraph {
             && eventProducerInfo.getInjectorPriority() == eventConsumerInfo.getInjectorPriority();
     }
 
-    private Optional<EventProducerInfo> resolveProducer(@NotNull String mixinClassName) {
+    private @NotNull List<EventProducerInfo> resolveProducers(@NotNull String mixinClassName) {
         return this.getProducers()
             .stream()
             .filter(it -> it.getDeclaringClassName().equals(mixinClassName))
-            .findFirst();
+            .toList();
     }
 
     @SuppressWarnings("CodeBlock2Expr")
     public @NotNull List<EventConsumerInfo> resolveConsumers(@NotNull String mixinClassName) {
-        return resolveProducer(mixinClassName)
-            .map(eventProducerInfo -> {
+        return this.resolveProducers(mixinClassName)
+            .stream()
+            .flatMap(eventProducerInfo -> {
                 return this.getConsumers()
                     .stream()
                     .filter(eventConsumerInfo -> matches(eventProducerInfo, eventConsumerInfo));
-            })
-            .orElseGet(() -> {
-                LogUtil.error("""
-                    [Missing Event Producer]
-                    There is no EventConsumerInfo found for mixin class {}.
-
-                    ◉ Solution: If you see this, you should create an issue in https://github.com/sakurawald/fuji/issues
-                    """);
-                return Stream.empty();
             })
             .toList();
     }
