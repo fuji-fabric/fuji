7fd01e3ad60a1437de4969a116f545067eb390e3
sakurawald <sakurawald@gmail.com>
2025-09-10 23:07:12 +0800

    refactor: remove AbstractCommandExecutionEvent

--- fabric/src/main/java/io/github/sakurawald/fuji/core/event/message/command/CommandExecutionPostEvent.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/event/message/command/CommandExecutionPostEvent.java
@@ -1,13 +1,21 @@
 package io.github.sakurawald.fuji.core.event.message.command;
 
+import io.github.sakurawald.fuji.core.event.message.BaseEvent;
 import java.util.Optional;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import net.minecraft.server.command.ServerCommandSource;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-public class CommandExecutionPostEvent extends AbstractCommandExecutionEvent {
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class CommandExecutionPostEvent extends BaseEvent {
 
-    public CommandExecutionPostEvent(@NotNull Object commandExecutor, @NotNull ServerCommandSource commandSource, @NotNull String commandString, @NotNull Optional<CallbackInfo> callback, @NotNull Optional<Integer> commandReturnValue) {
-        super(commandExecutor, commandSource, commandString, callback, commandReturnValue);
-    }
+    @NotNull Object commandExecutor;
+    @NotNull ServerCommandSource commandSource;
+    @NotNull String commandString;
+    @NotNull Optional<CallbackInfo> callback;
+    @NotNull Optional<Integer> commandReturnValue;
+
 }
--- fabric/src/main/java/io/github/sakurawald/fuji/core/event/message/command/CommandExecutionPreEvent.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/core/event/message/command/CommandExecutionPreEvent.java
@@ -1,15 +1,22 @@
 package io.github.sakurawald.fuji.core.event.message.command;
 
+import io.github.sakurawald.fuji.core.event.message.BaseEvent;
 import java.util.Optional;
-import lombok.ToString;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import net.minecraft.server.command.ServerCommandSource;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@ToString(callSuper = true)
-public class CommandExecutionPreEvent extends AbstractCommandExecutionEvent {
 
-    public CommandExecutionPreEvent(@NotNull Object commandExecutor, @NotNull ServerCommandSource commandSource, @NotNull String commandString, @NotNull Optional<CallbackInfo> callback, @NotNull Optional<Integer> commandReturnValue) {
-        super(commandExecutor, commandSource, commandString, callback, commandReturnValue);
-    }
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class CommandExecutionPreEvent extends BaseEvent {
+
+    @NotNull Object commandExecutor;
+    @NotNull ServerCommandSource commandSource;
+    @NotNull String commandString;
+    @NotNull Optional<CallbackInfo> callback;
+    @NotNull Optional<Integer> commandReturnValue;
+
 }
