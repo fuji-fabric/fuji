52decce37b2e6e6964da67f93fef12785feb2c5f
sakurawald <sakurawald@gmail.com>
2025-09-13 02:08:04 +0800

    refactor: extract BackService

--- fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/back/BackInitializer.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/back/BackInitializer.java
@@ -3,35 +3,23 @@ package io.github.sakurawald.fuji.module.initializer.back;
 import com.mojang.brigadier.context.CommandContext;
 import io.github.sakurawald.fuji.core.document.annotation.DocStringProvider;
 import io.github.sakurawald.fuji.core.document.annotation.Document;
-import io.github.sakurawald.fuji.core.auxiliary.ChronosUtil;
 import io.github.sakurawald.fuji.core.auxiliary.minecraft.CommandHelper;
-import io.github.sakurawald.fuji.core.auxiliary.minecraft.LuckpermsHelper;
 import io.github.sakurawald.fuji.core.auxiliary.minecraft.TextHelper;
 import io.github.sakurawald.fuji.core.command.annotation.CommandNode;
 import io.github.sakurawald.fuji.core.command.annotation.CommandRequirement;
 import io.github.sakurawald.fuji.core.command.annotation.CommandSource;
 import io.github.sakurawald.fuji.core.command.annotation.CommandTarget;
 import io.github.sakurawald.fuji.core.command.argument.wrapper.impl.Dimension;
-import io.github.sakurawald.fuji.core.command.exception.AbortCommandExecutionException;
 import io.github.sakurawald.fuji.core.config.handler.abst.BaseConfigurationHandler;
 import io.github.sakurawald.fuji.core.config.handler.impl.ObjectConfigurationHandler;
-import io.github.sakurawald.fuji.core.event.annotation.EventConsumer;
-import io.github.sakurawald.fuji.core.event.message.player.PlayerDeathEvent;
-import io.github.sakurawald.fuji.core.event.message.player.PlayerTeleportPreEvent;
-import io.github.sakurawald.fuji.core.structure.GlobalPos;
 import io.github.sakurawald.fuji.core.document.descriptor.MetaDescriptor;
 import io.github.sakurawald.fuji.module.initializer.ModuleInitializer;
 import io.github.sakurawald.fuji.module.initializer.back.config.model.BackConfigModel;
 import io.github.sakurawald.fuji.module.initializer.back.config.model.BackLocationHistoryModel;
+import io.github.sakurawald.fuji.module.initializer.back.service.BackService;
 import io.github.sakurawald.fuji.module.initializer.back.structure.LocationEntry;
-import io.github.sakurawald.fuji.module.initializer.back.structure.LocationHistory;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.Optional;
-import java.util.function.Function;
 
 @Document(id = 1751825568845L, value = """
     THis module allows the player to teleport back to:
@@ -42,29 +30,22 @@ public class BackInitializer extends ModuleInitializer {
 
     public static final BaseConfigurationHandler<BackConfigModel> config = ObjectConfigurationHandler.ofModule(BaseConfigurationHandler.CONFIG_JSON_LITERAL, BackConfigModel.class);
 
-    private static final BaseConfigurationHandler<BackLocationHistoryModel> savedPositionConfig = ObjectConfigurationHandler
+    public static final BaseConfigurationHandler<BackLocationHistoryModel> savedPositionConfig = ObjectConfigurationHandler
         .ofModule("location-history.json", BackLocationHistoryModel.class)
         .enableAutoSaveFeature();
 
     @DocStringProvider(id = 1751999540893L, value = """
         The max location entries to save for this player.
         """)
-    private static final MetaDescriptor<Integer> MAX_LOCATION_ENTRIES_TO_SAVE_META = new MetaDescriptor<>("fuji.back.max_location_entries_to_save", Integer::valueOf, 1751999540893L);
-
-    private static <R> R withLocationHistory(@NotNull ServerPlayerEntity player, Function<LocationHistory, R> function) {
-        String playerName = player.getGameProfile().getName();
-        savedPositionConfig.model().player2history.computeIfAbsent(playerName, k -> new LocationHistory());
-        LocationHistory locationHistory = savedPositionConfig.model().player2history.get(playerName);
-        return function.apply(locationHistory);
-    }
+    public static final MetaDescriptor<Integer> MAX_LOCATION_ENTRIES_TO_SAVE_META = new MetaDescriptor<>("fuji.back.max_location_entries_to_save", Integer::valueOf, 1751999540893L);
 
     @Document(id = 1751825574805L, value = "Push current location into the back location history.")
     @CommandNode("back push")
     @CommandRequirement(level = 4)
     private static int $push(@CommandSource @CommandTarget ServerPlayerEntity player) {
-        return withLocationHistory(player, locationHistory -> {
+        return BackService.withLocationHistory(player, locationHistory -> {
             LocationEntry locationEntry = LocationEntry.makeLocationEntry(player);
-            pushEntryAndTrim(player,locationHistory,locationEntry);
+            BackService.pushBackLocation(player,locationHistory,locationEntry);
             return CommandHelper.Return.SUCCESS;
         });
     }
@@ -73,7 +54,7 @@ public class BackInitializer extends ModuleInitializer {
     @CommandNode("back clear")
     @CommandRequirement(level = 4)
     private static int $clear(@CommandSource CommandContext<ServerCommandSource> source, @CommandTarget ServerPlayerEntity player) {
-        return withLocationHistory(player, locationHistory -> {
+        return BackService.withLocationHistory(player, locationHistory -> {
             locationHistory.clearEntries();
             TextHelper.sendTextByKey(source, "back.clear", player.getGameProfile().getName());
             return CommandHelper.Return.SUCCESS;
@@ -84,134 +65,37 @@ public class BackInitializer extends ModuleInitializer {
     @CommandNode("back list")
     @CommandRequirement(level = 4)
     private static int $list(@CommandSource CommandContext<ServerCommandSource> source, ServerPlayerEntity player) {
-        return backListWithParameters(source.getSource(), player);
+        return BackService.listBackLocations(source.getSource(), player);
     }
 
     @Document(id = 1751825593993L, value = "List the back location history.")
     @CommandNode("back list")
     private static int $list(@CommandSource ServerPlayerEntity source) {
-        return backListWithParameters(source.getCommandSource(), source);
-    }
-
-    private static Integer backListWithParameters(ServerCommandSource source, ServerPlayerEntity player) {
-        return withLocationHistory(player, locationHistory -> {
-            // Print header.
-            String targetPlayerName = player.getGameProfile().getName();
-            TextHelper.sendTextByKey(source, "back.list", targetPlayerName);
-
-            // Print body.
-            locationHistory
-                .listEntries()
-                .forEachRemaining(it -> {
-                    GlobalPos location = it.getLocation();
-                    TextHelper.sendTextByKey(source, "back.list.entry"
-                        , location.getLevel()
-                        , location.getX()
-                        , location.getY()
-                        , location.getZ()
-                        , ChronosUtil.Formatter.formatDate(it.getSavedTimestamp()));
-                });
-
-            return CommandHelper.Return.SUCCESS;
-        });
+        return BackService.listBackLocations(source.getCommandSource(), source);
     }
 
     @Document(id = 1751825598230L, value = "Back to the specified location.")
     @CommandNode("back")
     private static int $back(@CommandSource ServerPlayerEntity player) {
-        return backWithParameters(player, 1, null);
+        return BackService.teleportBackLocation(player, 1, null);
     }
 
     @Document(id = 1751825604578L, value = "Back to the specified location.")
     @CommandNode("back")
     private static int $back(@CommandSource ServerPlayerEntity player, int lastNLocation) {
-        return backWithParameters(player, lastNLocation, null);
+        return BackService.teleportBackLocation(player, lastNLocation, null);
     }
 
     @Document(id = 1751825608994L, value = "Back to the specified location.")
     @CommandNode("back")
     private static int $back(@CommandSource ServerPlayerEntity player, int lastNLocation, Dimension targetDimension) {
-        return backWithParameters(player, lastNLocation, targetDimension);
+        return BackService.teleportBackLocation(player, lastNLocation, targetDimension);
     }
 
     @Document(id = 1751825615959L, value = "Back to the specified location.")
     @CommandNode("back 1")
     private static int $back(@CommandSource ServerPlayerEntity player, Dimension targetDimension) {
-        return backWithParameters(player, 1, targetDimension);
+        return BackService.teleportBackLocation(player, 1, targetDimension);
     }
 
-    private static int backWithParameters(@NotNull ServerPlayerEntity player, int lastNLocation, @Nullable Dimension targetDimension) {
-        return withLocationHistory(player, locationHistory -> {
-            // find location entry.
-            LocationEntry latestEntry = locationHistory.findEntry(lastNLocation, targetDimension);
-            if (latestEntry == null) {
-                TextHelper.sendTextByKey(player, "back.no_previous_position");
-                throw new AbortCommandExecutionException();
-            }
-
-            // teleport with the location entry.
-            latestEntry.getLocation().teleport(player);
-            return CommandHelper.Return.SUCCESS;
-        });
-    }
-
-    private static int getMaxBackLocationEntriesToSave(@NotNull ServerPlayerEntity player) {
-        Optional<Integer> value = LuckpermsHelper.getMeta(player.getUuid(), MAX_LOCATION_ENTRIES_TO_SAVE_META);
-        return value.orElse(config.model().max_back_location_entries_to_save);
-    }
-
-    @SuppressWarnings("RedundantIfStatement")
-    private static boolean shouldSaveCurrentLocation(@NotNull ServerPlayerEntity player) {
-        return withLocationHistory(player, locationHistory -> {
-            LocationEntry latestEntry = locationHistory.getLatestEntry();
-
-            // Should save if there is no history entry before.
-            if (latestEntry == null) {
-                return true;
-            }
-
-            // Should not save location inside ignore distance.
-            GlobalPos latestLocation = latestEntry.getLocation();
-            double ignoreDistance = config.model().ignore_distance;
-            if (latestLocation.sameLevel(player.getWorld())
-                && player.getPos().squaredDistanceTo(latestLocation.getX(), latestLocation.getY(), latestLocation.getZ()) <= ignoreDistance * ignoreDistance
-            ) {
-                return false;
-            }
-
-            return true;
-        });
-    }
-
-    public static void trySaveCurrentLocation(@NotNull ServerPlayerEntity player) {
-        withLocationHistory(player, locationHistory -> {
-            if (shouldSaveCurrentLocation(player)) {
-                LocationEntry locationEntry = LocationEntry.makeLocationEntry(player);
-                pushEntryAndTrim(player, locationHistory, locationEntry);
-            }
-
-            return null;
-        });
-    }
-
-    private static void pushEntryAndTrim(@NotNull ServerPlayerEntity player, @NotNull LocationHistory locationHistory, @NotNull LocationEntry locationEntry) {
-        locationHistory.pushEntry(locationEntry);
-        locationHistory.trimEntries(getMaxBackLocationEntriesToSave(player));
-    }
-
-    @EventConsumer
-    private static void handleOnPlayerDeathEvent(PlayerDeathEvent event) {
-        if (config.model().enable_back_on_death) {
-            trySaveCurrentLocation(event.getPlayer());
-        }
-    }
-
-    @EventConsumer
-    private static void handlePlayerPreTeleportEvent(PlayerTeleportPreEvent event) {
-        if (event.getCallbackInfo().isCancelled()) return;
-
-        if (config.model().enable_back_on_teleport) {
-            trySaveCurrentLocation(event.getPlayer());
-        }
-    }
 }
---
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/back/service/BackService.java
@@ -0,0 +1,129 @@
+package io.github.sakurawald.fuji.module.initializer.back.service;
+
+import io.github.sakurawald.fuji.core.auxiliary.ChronosUtil;
+import io.github.sakurawald.fuji.core.auxiliary.minecraft.CommandHelper;
+import io.github.sakurawald.fuji.core.auxiliary.minecraft.LuckpermsHelper;
+import io.github.sakurawald.fuji.core.auxiliary.minecraft.TextHelper;
+import io.github.sakurawald.fuji.core.command.argument.wrapper.impl.Dimension;
+import io.github.sakurawald.fuji.core.command.exception.AbortCommandExecutionException;
+import io.github.sakurawald.fuji.core.event.annotation.EventConsumer;
+import io.github.sakurawald.fuji.core.event.message.player.PlayerDeathEvent;
+import io.github.sakurawald.fuji.core.event.message.player.PlayerTeleportPreEvent;
+import io.github.sakurawald.fuji.core.structure.GlobalPos;
+import io.github.sakurawald.fuji.module.initializer.back.BackInitializer;
+import io.github.sakurawald.fuji.module.initializer.back.structure.LocationEntry;
+import io.github.sakurawald.fuji.module.initializer.back.structure.LocationHistory;
+import java.util.Optional;
+import java.util.function.Function;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class BackService {
+
+    public static <R> R withLocationHistory(@NotNull ServerPlayerEntity player, Function<LocationHistory, R> function) {
+        String playerName = player.getGameProfile().getName();
+        BackInitializer.savedPositionConfig.model().player2history.computeIfAbsent(playerName, k -> new LocationHistory());
+        LocationHistory locationHistory = BackInitializer.savedPositionConfig.model().player2history.get(playerName);
+        return function.apply(locationHistory);
+    }
+
+    public static Integer listBackLocations(ServerCommandSource source, ServerPlayerEntity player) {
+        return withLocationHistory(player, locationHistory -> {
+            // Print header.
+            String targetPlayerName = player.getGameProfile().getName();
+            TextHelper.sendTextByKey(source, "back.list", targetPlayerName);
+
+            // Print body.
+            locationHistory
+                .listEntries()
+                .forEachRemaining(it -> {
+                    GlobalPos location = it.getLocation();
+                    TextHelper.sendTextByKey(source, "back.list.entry"
+                        , location.getLevel()
+                        , location.getX()
+                        , location.getY()
+                        , location.getZ()
+                        , ChronosUtil.Formatter.formatDate(it.getSavedTimestamp()));
+                });
+
+            return CommandHelper.Return.SUCCESS;
+        });
+    }
+
+    public static int teleportBackLocation(@NotNull ServerPlayerEntity player, int lastNLocation, @Nullable Dimension targetDimension) {
+        return withLocationHistory(player, locationHistory -> {
+            // find location entry.
+            LocationEntry latestEntry = locationHistory.findEntry(lastNLocation, targetDimension);
+            if (latestEntry == null) {
+                TextHelper.sendTextByKey(player, "back.no_previous_position");
+                throw new AbortCommandExecutionException();
+            }
+
+            // teleport with the location entry.
+            latestEntry.getLocation().teleport(player);
+            return CommandHelper.Return.SUCCESS;
+        });
+    }
+
+    private static int getMaxBackLocationEntriesToSave(@NotNull ServerPlayerEntity player) {
+        Optional<Integer> value = LuckpermsHelper.getMeta(player.getUuid(), BackInitializer.MAX_LOCATION_ENTRIES_TO_SAVE_META);
+        return value.orElse(BackInitializer.config.model().max_back_location_entries_to_save);
+    }
+
+    @SuppressWarnings("RedundantIfStatement")
+    private static boolean shouldSaveCurrentLocation(@NotNull ServerPlayerEntity player) {
+        return withLocationHistory(player, locationHistory -> {
+            LocationEntry latestEntry = locationHistory.getLatestEntry();
+
+            // Should save if there is no history entry before.
+            if (latestEntry == null) {
+                return true;
+            }
+
+            // Should not save location inside ignore distance.
+            GlobalPos latestLocation = latestEntry.getLocation();
+            double ignoreDistance = BackInitializer.config.model().ignore_distance;
+            if (latestLocation.sameLevel(player.getWorld())
+                && player.getPos().squaredDistanceTo(latestLocation.getX(), latestLocation.getY(), latestLocation.getZ()) <= ignoreDistance * ignoreDistance
+            ) {
+                return false;
+            }
+
+            return true;
+        });
+    }
+
+    private static void trySaveCurrentLocation(@NotNull ServerPlayerEntity player) {
+        withLocationHistory(player, locationHistory -> {
+            if (shouldSaveCurrentLocation(player)) {
+                LocationEntry locationEntry = LocationEntry.makeLocationEntry(player);
+                pushBackLocation(player, locationHistory, locationEntry);
+            }
+
+            return null;
+        });
+    }
+
+    public static void pushBackLocation(@NotNull ServerPlayerEntity player, @NotNull LocationHistory locationHistory, @NotNull LocationEntry locationEntry) {
+        locationHistory.pushEntry(locationEntry);
+        locationHistory.trimEntries(getMaxBackLocationEntriesToSave(player));
+    }
+
+    @EventConsumer
+    private static void handleOnPlayerDeathEvent(PlayerDeathEvent event) {
+        if (BackInitializer.config.model().enable_back_on_death) {
+            trySaveCurrentLocation(event.getPlayer());
+        }
+    }
+
+    @EventConsumer
+    private static void handlePlayerPreTeleportEvent(PlayerTeleportPreEvent event) {
+        if (event.getCallbackInfo().isCancelled()) return;
+
+        if (BackInitializer.config.model().enable_back_on_teleport) {
+            trySaveCurrentLocation(event.getPlayer());
+        }
+    }
+}
