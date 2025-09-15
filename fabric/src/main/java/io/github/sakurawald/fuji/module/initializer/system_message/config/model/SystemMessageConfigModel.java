59b7d389a48ec43a3edd490ae7811c3d080805cc
sakurawald <sakurawald@gmail.com>
2025-09-15 13:55:42 +0800

    feature: tweak the default config for system message

--- fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/system_message/config/model/SystemMessageConfigModel.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/initializer/system_message/config/model/SystemMessageConfigModel.java
@@ -14,24 +14,26 @@ public class SystemMessageConfigModel {
     List<SystemMessageRule> rules = new ArrayList<>() {
         {
             /* Join and leave. */
-            this.add(new SystemMessageRule(true, "Modify the style of player joined text.","multiplayer.player.joined", "<green>Player %s joined the server."));
-            this.add(new SystemMessageRule(true, "Cancel the sending of player left text.", "multiplayer.player.left", null));
+            this.add(new SystemMessageRule(false, "Modify the style of player joined text.", false, "multiplayer.player.joined", "<green>[+] Player %s joined the server."));
+            this.add(new SystemMessageRule(false, "Cancel the sending of player left text.", false, "multiplayer.player.left", null));
 
             /* Player death. */
-            this.add(new SystemMessageRule(true,"Modify the player death message.","death.attack.explosion.player","<rainbow>%1$s booooooom because of %2$s"));
+            this.add(new SystemMessageRule(false, "Modify the player death message.", false, "death.attack.explosion.player", "<rainbow>%1$s booooooom because of %2$s"));
+            this.add(new SystemMessageRule(false, "Modify the player death message.", false, "death.attack.fall", "<rb>%1$s hit the ground too hard"));
+            this.add(new SystemMessageRule(false, "Modify the player death message.", false, "death.fell.accident.generic", "<rb>%1$s fell from a high place"));
 
-            /* Vanilla Command Feedback. */
-            this.add(new SystemMessageRule(true, "Modify the style of `/seed` command feedback.", "commands.seed.success", "<rainbow>Seeeeeeeeeeed: %s"));
+            /* Vanilla Command Feedback */
+            this.add(new SystemMessageRule(true, "Modify the style of `/seed` command feedback.", false, "commands.seed.success", "<rainbow>Seeeeeeeeeeed: %s"));
 
-            /* Screen. */
-            this.add(new SystemMessageRule(true, "Modify the text displaying in the whitelist screen.","multiplayer.disconnect.not_whitelisted", "<rainbow>Please apply a whitelist first!"));
+            /* Whitelist Screen */
+            this.add(new SystemMessageRule(false, "Modify the text displaying in the whitelist screen.", true, "multiplayer.disconnect.not_whitelisted", "<rainbow>Please apply a whitelist first!"));
 
-            this.add(new SystemMessageRule(true, "Modify the text displaying in chest screen.","container.chest", "<rb>I see you opening the chest!"));
+            /* Ban Screen */
+            this.add(new SystemMessageRule(false, "Modify the text displaying in the ban screen.", true, "multiplayer.disconnect.banned", "<red><b><i>You are banned from this server"));
+            this.add(new SystemMessageRule(false, "Modify the text displaying in the ban screen.", true, "multiplayer.disconnect.banned.reason", "<red><b><i>You are banned from this server<newline><yellow>Reason: %s"));
 
-            this.add(new SystemMessageRule(true, "Modify the text displaying in the server closed screen", "multiplayer.disconnect.server_shutdown", "<red>Server closeeeeeeeed"));
-
-            this.add(new SystemMessageRule(true, "Modify the style of `/ban` command feedback.", "multiplayer.disconnect.banned", "<red>You are banned from this server"));
-            this.add(new SystemMessageRule(true, "Modify the style of `/ban` command feedback.", "multiplayer.disconnect.banned.reason", "<red>You are banned from this server<newline><yellow>Reason: %s"));
+            /* Container Screen */
+            this.add(new SystemMessageRule(false, "Modify the text displaying in chest screen.", true, "container.chest", "<rb>I see you opening the chest!"));
 
         }
     };
