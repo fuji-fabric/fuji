fbcf5efb19dfec1b0b1e9bbb42f073ef4a0c465d
sakurawald <sakurawald@gmail.com>
2025-09-20 23:30:28 +0800

    chore: fix the mixin target class for getWorldBorder()

--- fabric/src/main/java/io/github/sakurawald/fuji/module/mixin/world/border/BorderMixin.java
+++ fabric/src/main/java/io/github/sakurawald/fuji/module/mixin/world/border/BorderMixin.java
@@ -5,14 +5,19 @@ import io.github.sakurawald.fuji.core.annotation.HotPath;
 import io.github.sakurawald.fuji.core.auxiliary.minecraft.RegistryHelper;
 import io.github.sakurawald.fuji.module.initializer.world.border.WorldBorderInitializer;
 import io.github.sakurawald.fuji.module.initializer.world.border.structure.BorderDescriptor;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.border.WorldBorder;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
+#if MC_VER < MC_1_21_9
 @Mixin(World.class)
-public class WorldMixin {
+#elif MC_VER >= MC_1_21_9
+@Mixin(ServerWorld.class)
+#endif
+public class BorderMixin {
 
     @Unique
     @HotPath
