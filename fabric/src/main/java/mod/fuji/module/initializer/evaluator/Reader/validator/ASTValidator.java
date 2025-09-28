413b0b644a67d074838944a458078e1343395936
sakurawald <sakurawald@gmail.com>
2025-09-29 04:46:28 +0800

    feature: added ASTValidator

---
+++ fabric/src/main/java/mod/fuji/module/initializer/evaluator/Reader/validator/ASTValidator.java
@@ -0,0 +1,13 @@
+package mod.fuji.module.initializer.evaluator.Reader.validator;
+
+import java.util.List;
+import mod.fuji.module.initializer.evaluator.Reader.token.Token;
+import org.jetbrains.annotations.NotNull;
+
+public class ASTValidator {
+
+    public static void validate(@NotNull List<Token> AST) {
+
+    }
+
+}
