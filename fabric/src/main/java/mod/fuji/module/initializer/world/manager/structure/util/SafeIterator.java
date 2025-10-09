6ada30880db73f60bd50e101670a4d8fc7567e39
sakurawald <sakurawald@gmail.com>
2025-10-09 09:48:20 +0800

    feature: rewrite the world injection point using expression.

--- fabric/src/main/java/mod/fuji/module/initializer/world/manager/structure/util/SafeIterator.java
+++ fabric/src/main/java/mod/fuji/module/initializer/world/manager/structure/util/SafeIterator.java
@@ -1,16 +1,18 @@
 package mod.fuji.module.initializer.world.manager.structure.util;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 public final class SafeIterator<T> implements Iterator<T> {
     private final Object @NotNull [] values;
     private int index = 0;
 
-    public SafeIterator(@NotNull Collection<T> source) {
-        this.values = source.toArray();
+    public SafeIterator(@NotNull Iterator<T> source) {
+        List<T> list = new ArrayList<>();
+        source.forEachRemaining(list::add);
+        this.values = list.toArray();
     }
 
     @Override
--- fabric/src/main/java/mod/fuji/module/mixin/world/manager/tick/MinecraftServerMixin.java
+++ fabric/src/main/java/mod/fuji/module/mixin/world/manager/tick/MinecraftServerMixin.java
@@ -1,7 +1,8 @@
 package mod.fuji.module.mixin.world.manager.tick;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import mod.fuji.module.initializer.world.manager.structure.util.SafeIterator;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
@@ -9,16 +10,19 @@ import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-import java.util.Collection;
 import java.util.Iterator;
 
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin {
 
     @SuppressWarnings("unchecked")
-    @WrapOperation(method = "tickWorlds", at = @At(value = "INVOKE", target = "Ljava/lang/Iterable;iterator()Ljava/util/Iterator;", ordinal = 0), require = 0)
-    private @NotNull Iterator<ServerWorld> fuji$copyBeforeTicking(Iterable<?> instance, Operation<Iterator<?>> original) {
+    @ModifyExpressionValue(method = "tickWorlds", at = @At("MIXINEXTRAS:EXPRESSION"))
+    @Expression("this.getWorlds().iterator()")
+    @Definition(id = "getWorlds", method = "Lnet/minecraft/server/MinecraftServer;getWorlds()Ljava/lang/Iterable;")
+    @Definition(id = "iterator", method = "Ljava/lang/Iterable;iterator()Ljava/util/Iterator;")
+    @NotNull Iterator<ServerWorld> copyBeforeTickWorlds(Iterator<?> original) {
         // NOTE: After issue /world reset, it's possible that all the worlds will be ticked twice.
-        return new SafeIterator<>((Collection<ServerWorld>) instance);
+        return new SafeIterator<>((Iterator<ServerWorld>) original);
     }
+
 }
