f0deeeec1a75081a2cda480f65d4f7dfd1e419b5
sakurawald <sakurawald@gmail.com>
2025-12-12 03:51:00 +0800

    build: try pass the CI x 3

--- fabric/src/main/java/mod/fuji/core/structure/BuiltinDimensionTypesIR.java
+++ fabric/src/main/java/mod/fuji/core/structure/BuiltinDimensionTypesIR.java
@@ -2,38 +2,24 @@ package mod.fuji.core.structure;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.dimension.DimensionType;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
 
 @Getter
 @AllArgsConstructor
 public enum BuiltinDimensionTypesIR {
 
     OVERWORLD(
-        #if MC_VER < MC_1_21_11
-        net.minecraft.world.level.dimension.BuiltinDimensionTypes.OVERWORLD_EFFECTS
-        #elif MC_VER >= MC_1_21_11
-        net.minecraft.world.level.dimension.BuiltinDimensionTypes.OVERWORLD
-        #endif
+        IdentifierIR.of(BuiltinDimensionTypes.OVERWORLD_EFFECTS)
     ),
     NETHER(
-        #if MC_VER < MC_1_21_11
-        net.minecraft.world.level.dimension.BuiltinDimensionTypes.NETHER_EFFECTS
-        #elif MC_VER >= MC_1_21_11
-        net.minecraft.world.level.dimension.BuiltinDimensionTypes.NETHER
-        #endif
+        IdentifierIR.of(BuiltinDimensionTypes.NETHER_EFFECTS)
     ),
     END(
-        #if MC_VER < MC_1_21_11
-        net.minecraft.world.level.dimension.BuiltinDimensionTypes.END_EFFECTS
-        #elif MC_VER >= MC_1_21_11
-        net.minecraft.world.level.dimension.BuiltinDimensionTypes.END
-        #endif
+        IdentifierIR.of(BuiltinDimensionTypes.END_EFFECTS)
     );
 
     @SuppressWarnings("ImmutableEnumChecker")
-    final ResourceKey<@NotNull DimensionType> nativeValue;
+    final IdentifierIR nativeValue;
 
     @Override
     public String toString() {
