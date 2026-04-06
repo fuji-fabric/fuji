e4594fcc6716bdfe324fb11cb4b3d8bcb13f24d9
sakurawald <sakurawald@gmail.com>
2026-04-06 22:05:19 +0800

    chore: fix the RegistryLoaderMixin

--- fabric/src/main/java/mod/fuji/module/mixin/chat/style/RegistryLoaderMixin.java
+++ fabric/src/main/java/mod/fuji/module/mixin/chat/style/RegistryLoaderMixin.java
@@ -25,10 +25,14 @@ import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.asm.mixin.injection.Coerce;
 import com.llamalad7.mixinextras.sugar.Local;
 import org.jetbrains.annotations.NotNull;
-#elif MC_VER > MC_1_21
+#elif MC_VER > MC_1_21 && MC_VER < MC_26_1
 import org.spongepowered.asm.mixin.injection.Coerce;
-import net.minecraft.resources.RegistryOps;
 import net.minecraft.core.HolderLookup;
+#elif MC_VER >= MC_26_1
+import org.spongepowered.asm.mixin.Unique;
+import com.llamalad7.mixinextras.sugar.Local;
+import java.util.concurrent.CompletableFuture;
+import net.minecraft.resources.RegistryLoadTask;
 #endif
 
 @SuppressWarnings({"unchecked"})
@@ -55,7 +59,7 @@ public class RegistryLoaderMixin {
         , List<RegistryDataLoader.RegistryData<?>> entries
         , CallbackInfoReturnable<RegistryAccess.Frozen> cir
         , @Local(ordinal = 1) @NotNull List<RegistryDataLoader.Loader<?>> iterable)
-    #elif MC_VER > MC_1_21
+    #elif MC_VER > MC_1_21 && MC_VER < MC_26_1
     @Inject(method = "load(Lnet/minecraft/resources/RegistryDataLoader$LoadingFunction;Ljava/util/List;Ljava/util/List;)Lnet/minecraft/core/RegistryAccess$Frozen;"
         , at = @At(value = "INVOKE", target = "Ljava/util/List;forEach(Ljava/util/function/Consumer;)V", ordinal = 0, shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
     private static void registerNewMessageType(@Coerce Object registryLoadable
@@ -66,24 +70,38 @@ public class RegistryLoaderMixin {
         , List<RegistryDataLoader.Loader<?>> iterable
         , RegistryOps.RegistryInfoLookup registryInfoGetter
     )
+    #elif MC_VER >= MC_26_1
+    @Inject(method = "lambda$load$0"
+        , at = @At(value = "INVOKE", target = "Lnet/minecraft/resources/RegistryDataLoader;createContext(Ljava/util/List;Ljava/util/List;)Lnet/minecraft/resources/RegistryOps$RegistryInfoLookup;"))
+    private static void registerNewMessageType(
+        CallbackInfoReturnable<CompletableFuture<RegistryAccess.Frozen>> cir,
+        @Local(ordinal = 2) List<RegistryLoadTask<?>> iterable
+    )
     #endif
     {
         for (var loader : iterable) {
             #if MC_VER <= MC_1_20_4
             WritableRegistry<?> registry = loader.getFirst();
-            #elif MC_VER > MC_1_20_4
+            #elif MC_VER > MC_1_20_4 && MC_VER < MC_26_1
             WritableRegistry<?> registry = loader.registry();
+            #elif MC_VER >= MC_26_1
+            WritableRegistry<?> registry = loader.registry;
             #endif
 
-            /* Register out custom message type in the Registry<MessageType> instance. */
-            ResourceKey<? extends Registry<?>> registryKey = registry.key();
-            if (registryKey.equals(Registries.CHAT_TYPE)) {
-                Registry<ChatType> messageTypeRegistry = (Registry<ChatType>) registry;
+            tryPatchRegistry(registry);
+        }
+    }
 
-                // NOTE: in single-player world, the MESSAGE_TYPE_KEY will be registered twice, causing a `network protocol error` while join the world.
-                if (!messageTypeRegistry.containsKey(ChatStyleInitializer.MESSAGE_TYPE_KEY)) {
-                    Registry.register(messageTypeRegistry, ChatStyleInitializer.MESSAGE_TYPE_KEY, ChatStyleInitializer.MESSAGE_TYPE_VALUE);
-                }
+    @Unique
+    private static void tryPatchRegistry(WritableRegistry<?> registry) {
+        /* Register out custom message type in the Registry<MessageType> instance. */
+        ResourceKey<? extends Registry<?>> registryKey = registry.key();
+        if (registryKey.equals(Registries.CHAT_TYPE)) {
+            Registry<ChatType> messageTypeRegistry = (Registry<ChatType>) registry;
+
+            // NOTE: in single-player world, the MESSAGE_TYPE_KEY will be registered twice, causing a `network protocol error` while join the world.
+            if (!messageTypeRegistry.containsKey(ChatStyleInitializer.MESSAGE_TYPE_KEY)) {
+                Registry.register(messageTypeRegistry, ChatStyleInitializer.MESSAGE_TYPE_KEY, ChatStyleInitializer.MESSAGE_TYPE_VALUE);
             }
         }
     }
