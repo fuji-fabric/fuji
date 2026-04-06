27c246b94a1d477b35085c6ca8b8a7c74bd15808
sakurawald <sakurawald@gmail.com>
2026-04-06 17:05:48 +0800

    chore: introduce duck type for SimpleGui type.

---
+++ fabric/src/main/java/mod/fuji/core/gui/structure/SimpleGuiDuck.java
@@ -0,0 +1,30 @@
+package mod.fuji.core.gui.structure;
+
+
+import eu.pb4.sgui.api.gui.SimpleGui;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.inventory.MenuType;
+
+public class SimpleGuiDuck extends SimpleGui implements SlotGuiInterfaceDuck
+{
+    public SimpleGuiDuck(MenuType<?> type, ServerPlayer player, boolean manipulatePlayerSlots) {
+        super(type, player, manipulatePlayerSlots);
+    }
+
+    public void onVirtualGuiClose() {
+
+    }
+
+    #if MC_VER < MC_26_1
+    @Override
+    public final void onClose() {
+        this.onVirtualGuiClose();
+    }
+    #elif MC_VER >= MC_26_1
+    @Override
+    public final void onManualClose() {
+        this.onVirtualGuiClose();
+    }
+    #endif
+
+}
