60373675faa86fd6be4fe491757446b29c7d4953
sakurawald <sakurawald@gmail.com>
2026-05-18 02:53:08 +0800

    feature: only colorize the sign when it's directly edited by a player.

--- fabric/src/main/java/mod/fuji/module/mixin/color/sign/SignBlockEntityMixin.java
+++ fabric/src/main/java/mod/fuji/module/mixin/color/sign/SignBlockEntityMixin.java
@@ -1,6 +1,11 @@
 package mod.fuji.module.mixin.color.sign;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
 import mod.fuji.core.auxiliary.minecraft.PlayerHelper;
 import mod.fuji.core.auxiliary.minecraft.ServerHelper;
 import mod.fuji.core.auxiliary.minecraft.TextHelper;
@@ -9,15 +14,14 @@ import mod.fuji.core.structure.GlobalBlockPos;
 import mod.fuji.module.initializer.color.ColorInitializer;
 import mod.fuji.module.initializer.color.sign.ColorSignInitializer;
 import mod.fuji.module.initializer.color.sign.structure.SignCache;
-import java.util.concurrent.atomic.AtomicReference;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.entity.SignText;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.network.chat.Component;
-import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -25,11 +29,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Mixin(SignBlockEntity.class)
 public abstract class SignBlockEntityMixin extends BlockEntity {
@@ -44,48 +43,50 @@ public abstract class SignBlockEntityMixin extends BlockEntity {
     @ModifyVariable(method = "setText", at = @At("HEAD"), argsOnly = true)
     @NotNull
     SignText processInputLineStrings(@NotNull SignText signText, @Local(argsOnly = true) boolean isFront) {
-        // NOTE: Only process the sign text when there is a logic server in current session.
+        /* Only process the sign text when there is a logic server in current session. */
         if (ServerHelper.getServer() == null) return signText;
         if (!WorldHelper.isServerWorld(this.level)) return signText;
 
-        /* Process input line texts. */
-        Component[] inputLineTexts = signText.getMessages(false);
-        Component[] outputLineTexts = new Component[inputLineTexts.length];
-        String[] inputLineStrings = new String[inputLineTexts.length];
+        /* Only process the sign text when it's directly edited by a player. */
+        return Optional
+            .ofNullable(getPlayerWhoMayEdit())
+            .map(editingPlayerUUID -> {
+                /* Process input line texts. */
+                Component[] inputLineTexts = signText.getMessages(false);
+                Component[] outputLineTexts = new Component[inputLineTexts.length];
+                String[] inputLineStrings = new String[inputLineTexts.length];
 
-        for (int i = 0; i < inputLineTexts.length; i++) {
-            /* Map the line text into line string. */
-            // NOTE: The inputLineTexts[i] may be null if you write nothing.
-            if (inputLineTexts[i] == null) {
-                inputLineTexts[i] = Component.literal("");
-            }
-            AtomicReference<String> inputLineString = new AtomicReference<>(TextHelper.Operators.getString(inputLineTexts[i]));
+                for (int i = 0; i < inputLineTexts.length; i++) {
+                    /* Map the line text into line string. */
+                    // NOTE: The inputLineTexts[i] may be null if you write nothing.
+                    if (inputLineTexts[i] == null) {
+                        inputLineTexts[i] = Component.literal("");
+                    }
+                    AtomicReference<String> inputLineString = new AtomicReference<>(TextHelper.Operators.getString(inputLineTexts[i]));
 
-            /* Rewrite the input line string. */
-            inputLineString.getAndUpdate(ColorInitializer::rewriteColorCodes);
+                    /* Rewrite the input line string. */
+                    inputLineString.getAndUpdate(ColorInitializer::rewriteColorCodes);
 
-            /* Stripe style tags. */
-            if (ColorSignInitializer.config.model().requires_corresponding_permission_to_use_style_tag) {
-                Optional
-                    .ofNullable(getPlayerWhoMayEdit())
-                    .ifPresent(editorUUID -> {
-                        Optional<ServerPlayer> editingPlayer = PlayerHelper.Lookup.getOnlinePlayerByUuid(editorUUID);
+                    /* Stripe style tags. */
+                    if (ColorSignInitializer.config.model().requires_corresponding_permission_to_use_style_tag) {
+                        Optional<ServerPlayer> editingPlayer = PlayerHelper.Lookup.getOnlinePlayerByUuid(editingPlayerUUID);
                         editingPlayer.ifPresent(player -> {
                             inputLineString.getAndUpdate(it -> ColorSignInitializer.stripeStyleTags(player, it));
                         });
-                    });
-            }
+                    }
 
-            /* Update the final line texts and line strings. */
-            outputLineTexts[i] = TextHelper.Parsers.parseString(TextHelper.Parsers.MINI_MESSAGE_ONLY_PARSER, inputLineString.get());
-            inputLineStrings[i] = inputLineString.get();
-        }
+                    /* Update the final line texts and line strings. */
+                    outputLineTexts[i] = TextHelper.Parsers.parseString(TextHelper.Parsers.MINI_MESSAGE_ONLY_PARSER, inputLineString.get());
+                    inputLineStrings[i] = inputLineString.get();
+                }
 
-        /* Write sign cache. */
-        writeSignCache(isFront, inputLineStrings);
+                /* Write sign cache. */
+                writeSignCache(isFront, inputLineStrings);
 
-        /* Return the output line texts. */
-        return new SignText(outputLineTexts, outputLineTexts, signText.getColor(), signText.hasGlowingText());
+                /* Return the output line texts. */
+                return new SignText(outputLineTexts, outputLineTexts, signText.getColor(), signText.hasGlowingText());
+            })
+            .orElse(signText);
     }
 
     @Unique
