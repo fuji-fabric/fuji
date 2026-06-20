69383c04cba6bdc4038c95b829385583962d1006
sakurawald <sakurawald@gmail.com>
2026-06-21 00:51:09 +0800

    fix: fail to fire the world lifecycle events due to the symbol signature change from fabric-api

--- fabric/src/main/java/mod/fuji/core/auxiliary/minecraft/FabricApiHelper.java
+++ fabric/src/main/java/mod/fuji/core/auxiliary/minecraft/FabricApiHelper.java
@@ -1,9 +1,10 @@
 package mod.fuji.core.auxiliary.minecraft;
 
-import mod.fuji.core.auxiliary.LogUtil;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
+import mod.fuji.core.auxiliary.LogUtil;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
@@ -12,7 +13,6 @@ import org.jetbrains.annotations.NotNull;
 public class FabricApiHelper {
 
     private static final String FABRIC_API_EVENT_INVOKER_METHOD_NAME = "invoker";
-    private static final String SERVER_WORLD_EVENTS_CLASS_NAME = "net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents";
 
     private static boolean isFabricApiModInstalled() {
         return FabricLoader.getInstance().isModLoaded("fabric-api");
@@ -21,13 +21,16 @@ public class FabricApiHelper {
     public static void fireOnWorldLoadEvent(@NotNull MinecraftServer server, @NotNull ServerLevel serverWorld) throws Exception {
         List<Class<?>> eventParameters = List.of(MinecraftServer.class, ServerLevel.class);
         List<Object> eventArguments = List.of(server, serverWorld);
-        FabricApiHelper.fireEvent(FabricApiHelper.SERVER_WORLD_EVENTS_CLASS_NAME, "LOAD", "onWorldLoad", eventParameters, eventArguments);
+
+        FabricApiHelper.fireEvent("net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents", "LOAD", "onWorldLoad", eventParameters, eventArguments);
+        FabricApiHelper.fireEvent("net.fabricmc.fabric.api.event.lifecycle.v1.ServerLevelEvents", "LOAD", "onLevelLoad", eventParameters, eventArguments);
     }
 
     public static void fireOnWorldUnloadEvent(@NotNull MinecraftServer server, @NotNull ServerLevel serverWorld) throws Exception {
         List<Class<?>> eventParameters = List.of(MinecraftServer.class, ServerLevel.class);
         List<Object> eventArguments = List.of(server, serverWorld);
-        FabricApiHelper.fireEvent(FabricApiHelper.SERVER_WORLD_EVENTS_CLASS_NAME, "UNLOAD", "onWorldUnload", eventParameters, eventArguments);
+        FabricApiHelper.fireEvent("net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents", "UNLOAD", "onWorldUnload", eventParameters, eventArguments);
+        FabricApiHelper.fireEvent("net.fabricmc.fabric.api.event.lifecycle.v1.ServerLevelEvents", "UNLOAD", "onLevelUnload", eventParameters, eventArguments);
     }
 
     @SuppressWarnings("SameParameterValue")
@@ -39,35 +42,49 @@ public class FabricApiHelper {
             return;
         }
 
-        /* Get the event declaring class. */
-        Class<?> eventDeclaringClass = Class.forName(eventDeclaringClassName);
-        LogUtil.debug("fireEvent(): Class<?> eventDeclaringClass = {}", eventDeclaringClass);
 
-        /* Get the event field. */
-        Field eventDeclaringField = eventDeclaringClass.getDeclaredField(eventDeclaringFieldName);
-        LogUtil.debug("fireEvent(): Field eventDeclaringField = {}", eventDeclaringField);
+        try {
+            /* Get the event declaring class. */
+            Class<?> eventDeclaringClass = Class.forName(eventDeclaringClassName);
+            LogUtil.debug("fireEvent(): Class<?> eventDeclaringClass = {}", eventDeclaringClass);
 
-        /* Get the event class. */
-        eventDeclaringField.setAccessible(true);
-        Object eventInstance = eventDeclaringField.get(null);
-        Class<?> eventClass = eventInstance.getClass();
-        LogUtil.debug("fireEvent(): Object eventInstance = {}", eventInstance);
+            /* Get the event field. */
+            Field eventDeclaringField = eventDeclaringClass.getDeclaredField(eventDeclaringFieldName);
+            LogUtil.debug("fireEvent(): Field eventDeclaringField = {}", eventDeclaringField);
 
-        /* Get the invoker() method and call it. */
-        Method invokerMethod = eventClass.getMethod(FABRIC_API_EVENT_INVOKER_METHOD_NAME);
-        LogUtil.debug("fireEvent(): Method invokerMethod = {}", invokerMethod);
+            /* Get the event class. */
+            eventDeclaringField.setAccessible(true);
+            Object eventInstance = eventDeclaringField.get(null);
+            Class<?> eventClass = eventInstance.getClass();
+            LogUtil.debug("fireEvent(): Object eventInstance = {}", eventInstance);
 
-        Object eventInvokerInstance = invokerMethod.invoke(eventInstance);
-        Class<?> eventInvokerClass = eventInvokerInstance.getClass();
-        LogUtil.debug("fireEvent(): Object eventInvokerInstance = {}", eventInvokerInstance);
+            /* Get the invoker() method and call it. */
+            Method invokerMethod = eventClass.getMethod(FABRIC_API_EVENT_INVOKER_METHOD_NAME);
+            LogUtil.debug("fireEvent(): Method invokerMethod = {}", invokerMethod);
 
-        /* Get the event fire method. */
-        Class<?>[] eventFireMethodParameterArray = eventFireMethodParameterList.toArray(new Class[0]);
-        Method eventFireMethod = eventInvokerClass.getMethod(eventFireMethodName, eventFireMethodParameterArray);
+            Object eventInvokerInstance = invokerMethod.invoke(eventInstance);
+            Class<?> eventInvokerClass = eventInvokerInstance.getClass();
+            LogUtil.debug("fireEvent(): Object eventInvokerInstance = {}", eventInvokerInstance);
 
-        /* Invoke the fire method. */
-        Object[] eventFireMethodArgumentArray = eventFireMethodArgumentList.toArray(new Object[0]);
-        eventFireMethod.setAccessible(true);
-        eventFireMethod.invoke(eventInvokerInstance, eventFireMethodArgumentArray);
+            /* Get the event fire method. */
+            Class<?>[] eventFireMethodParameterArray = eventFireMethodParameterList.toArray(new Class[0]);
+            Method eventFireMethod = eventInvokerClass.getMethod(eventFireMethodName, eventFireMethodParameterArray);
+
+            /* Invoke the fire method. */
+            Object[] eventFireMethodArgumentArray = eventFireMethodArgumentList.toArray(new Object[0]);
+            eventFireMethod.setAccessible(true);
+            eventFireMethod.invoke(eventInvokerInstance, eventFireMethodArgumentArray);
+        } catch (ClassNotFoundException | InvocationTargetException | NoSuchMethodException | IllegalAccessException |
+                 IllegalArgumentException | NoSuchFieldException error) {
+            LogUtil.debug("""
+                Failed to fire the specified event, now ignoring it.
+                ◉ Event Declaring Class Name: {}
+                ◉ Event Declaring Field Name: {}
+                ◉ Event Declaring Method Name: {}
+                ◉ Event Fire Method Parameter List: {}
+                ◉ Event Fire Method Argument List: {}
+                ◉ Error: {}
+                """, eventDeclaringClassName, eventDeclaringFieldName, eventFireMethodName, eventFireMethodParameterList, eventFireMethodArgumentList, error);
+        }
     }
 }
